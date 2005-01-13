96563e690073ac72d93cb0475c3d0d137d54e87d
Hamilton Verissimo <hammett@gmail.com>
2005-01-13 16:57:01 +0000

    Batch Registration facility
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@188 73e77b4c-caa6-f847-a29a-24ab75ae54b6

---
+++ Facilities/BatchRegistration/Castle.Facilities.BatchRegistration.Tests/BatchRegistrationFacilityTestCase.cs
@@ -0,0 +1,136 @@
+// Copyright 2004 DigitalCraftsmen - http://www.digitalcraftsmen.com.br/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Facilities.BatchRegistration.Tests
+{
+	using System;
+	using System.IO;
+	using System.Configuration;
+
+	using NUnit.Framework;
+
+	using Castle.MicroKernel;
+
+	using Castle.Windsor.Configuration.Xml;
+
+	using Castle.Facilities.BatchRegistration.Tests.Components;
+
+	[TestFixture]
+	public class BatchRegistrationFacilityTestCase
+	{
+		private IKernel _kernel;
+
+		[SetUp]
+		public void Init()
+		{
+			_kernel = new DefaultKernel();
+		}
+
+		[TearDown]
+		public void Finish()
+		{
+			_kernel.Dispose();
+		}
+
+		[Test]
+		public void UsingAttributes()
+		{
+			String xml = 
+				"<configuration>" + 
+				"	<facilities>" + 
+				"		<facility id=\"batchregistration\">" + 
+				"			<assemblyBatch name=\"Castle.Facilities.BatchRegistration.Tests\" useAttributes=\"true\" />" + 
+				"		</facility>" + 
+				"	</facilities>" + 
+				"</configuration>";
+
+			_kernel.ConfigurationStore = new XmlConfigurationStore( new StringReader(xml) );
+
+			_kernel.AddFacility( "batchregistration", new BatchRegistrationFacility() );
+
+			Assert.IsTrue( _kernel.HasComponent("comp1") );
+			Assert.IsTrue( _kernel.HasComponent("comp2") );
+			Assert.IsTrue( _kernel.HasComponent( typeof(Component1) ) );
+			Assert.IsTrue( _kernel.HasComponent( typeof(Component2) ) );
+		}
+
+		[Test]
+		public void UsingAttributesWithExcludes()
+		{
+			String xml = 
+				"<configuration>" + 
+				"	<facilities>" + 
+				"		<facility id=\"batchregistration\">" + 
+				"			<assemblyBatch name=\"Castle.Facilities.BatchRegistration.Tests\" useAttributes=\"true\" >" + 
+				"				<exclude type=\"Castle.Facilities.BatchRegistration.Tests.Components.Component2\" />" + 
+				"			</assemblyBatch>" + 
+				"		</facility>" + 
+				"	</facilities>" + 
+				"</configuration>";
+
+			_kernel.ConfigurationStore = new XmlConfigurationStore( new StringReader(xml) );
+
+			_kernel.AddFacility( "batchregistration", new BatchRegistrationFacility() );
+
+			Assert.IsTrue( _kernel.HasComponent("comp1") );
+			Assert.IsFalse( _kernel.HasComponent("comp2") );
+			Assert.IsTrue( _kernel.HasComponent( typeof(Component1) ) );
+			Assert.IsFalse( _kernel.HasComponent( typeof(Component2) ) );
+		}
+
+		[Test]
+		public void Includes()
+		{
+			String xml = 
+				"<configuration>" + 
+				"	<facilities>" + 
+				"		<facility id=\"batchregistration\">" + 
+				"			<assemblyBatch name=\"Castle.Facilities.BatchRegistration.Tests\" useAttributes=\"false\" >" + 
+				"				<include key=\"other\" component=\"Castle.Facilities.BatchRegistration.Tests.Components.OtherComponent\" />" + 
+				"			</assemblyBatch>" + 
+				"		</facility>" + 
+				"	</facilities>" + 
+				"</configuration>";
+
+			_kernel.ConfigurationStore = new XmlConfigurationStore( new StringReader(xml) );
+
+			_kernel.AddFacility( "batchregistration", new BatchRegistrationFacility() );
+
+			Assert.IsTrue( _kernel.HasComponent("other") );
+			Assert.IsFalse( _kernel.HasComponent("comp2") );
+			Assert.IsTrue( _kernel.HasComponent( typeof(OtherComponent) ) );
+			Assert.IsFalse( _kernel.HasComponent( typeof(Component2) ) );
+		}
+
+		[Test]
+		[ExpectedException( typeof(ConfigurationException) )]
+		public void InvalidAssemblyName()
+		{
+			String xml = 
+				"<configuration>" + 
+				"	<facilities>" + 
+				"		<facility id=\"batchregistration\">" + 
+				"			<assemblyBatch name=\"MyCastle.Facilities.BatchRegistration.Tests\" useAttributes=\"false\" >" + 
+				"				<include key=\"other\" component=\"Castle.Facilities.BatchRegistration.Tests.Components.OtherComponent\" />" + 
+				"			</assemblyBatch>" + 
+				"		</facility>" + 
+				"	</facilities>" + 
+				"</configuration>";
+
+			_kernel.ConfigurationStore = new XmlConfigurationStore( new StringReader(xml) );
+
+			_kernel.AddFacility( "batchregistration", new BatchRegistrationFacility() );
+		}
+	}
+}
---
+++ Facilities/BatchRegistration/Castle.Facilities.BatchRegistration/BatchRegistrationFacility.cs
@@ -0,0 +1,102 @@
+// Copyright 2004 DigitalCraftsmen - http://www.digitalcraftsmen.com.br/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Facilities.BatchRegistration
+{
+	using System;
+	using System.Configuration;
+
+	using Castle.Model.Configuration;
+
+	using Castle.MicroKernel.Facilities;
+
+	public class BatchRegistrationFacility : AbstractFacility
+	{
+		protected override void Init()
+		{
+			if (FacilityConfig == null) return;
+
+			foreach(IConfiguration config in FacilityConfig.Children)
+			{
+				AddComponents(config);
+			}
+		}
+
+		private void AddComponents(IConfiguration config)
+		{
+			if (!"assemblyBatch".Equals(config.Name))
+			{
+				throw new ConfigurationException("Invalid node inside facility configuration. " + 
+					"Expected assemblyBatch");
+			}
+
+			String assemblyName = config.Attributes["name"];
+
+			if (assemblyName == null || assemblyName.Length == 0)
+			{
+				throw new ConfigurationException("The assemblyBatch node must have a 'name' " + 
+					" attribute with the name of the assembly");
+			}
+
+			ComponentScanner scanner = new ComponentScanner(assemblyName);
+
+			ConfigureScanner(config, scanner);
+
+			ComponentDefinition[] definitions = scanner.Process();
+
+			foreach(ComponentDefinition definition in definitions)
+			{
+				if (definition.ServiceType == null)
+				{
+					Kernel.AddComponent( definition.Key, definition.ClassType );
+				}
+				else
+				{
+					Kernel.AddComponent( definition.Key, definition.ServiceType, definition.ClassType );
+				}
+			}
+		}
+
+		private void ConfigureScanner(IConfiguration config, ComponentScanner scanner)
+		{
+			if (config.Attributes["useAttributes"] != null)
+			{
+				scanner.UseAttributes = config.Attributes["useAttributes"].Equals("true");
+			}
+	
+			foreach(IConfiguration innerConfig in config.Children)
+			{
+				if ("include".Equals(innerConfig.Name) )
+				{
+					String key = innerConfig.Attributes["key"];
+					String service = innerConfig.Attributes["service"];
+					String component = innerConfig.Attributes["component"];
+
+					scanner.AddInclude( key, service, component );
+				}
+				else if ("exclude".Equals(innerConfig.Name) )
+				{
+					String type = innerConfig.Attributes["type"];
+
+					scanner.AddExclude( type );
+				}
+				else
+				{
+					throw new ConfigurationException("Invalid node inside assemblyBatch " + 
+						"configuration. Expected 'include' or 'exclude'");
+				}
+			}
+		}
+	}	
+}
