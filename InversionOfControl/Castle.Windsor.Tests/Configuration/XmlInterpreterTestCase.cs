be9103cee9f93528b759e7ad7f6a095f44f67460
Craig Neuwirt <cneuwirt@gmail.com>
2005-03-07 19:23:44 +0000

    Added ManifestResourceSource to retrieve configurations from an embedded assembly resource.
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@300 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- InversionOfControl/Castle.Windsor.Tests/Configuration/XmlInterpreterTestCase.cs
+++ InversionOfControl/Castle.Windsor.Tests/Configuration/XmlInterpreterTestCase.cs
@@ -24,6 +24,8 @@ namespace Castle.Windsor.Tests.Configuration
 	using Castle.MicroKernel.SubSystems.Configuration;
 
 	using Castle.Windsor.Configuration.Interpreters;
+	using Castle.Windsor.Configuration.Sources;
+
 
 	/// <summary>
 	/// Summary description for XmlInterpreterTestCase.
@@ -71,6 +73,47 @@ namespace Castle.Windsor.Tests.Configuration
 			WindsorContainer container = new WindsorContainer(store);			
 
 			container.AddFacility("testidengine", new DummyFacility());
+		}
+
+		[Test]
+		public void ProperManifestDeserialization()
+		{
+			DefaultConfigurationStore store = new DefaultConfigurationStore();
+			XmlInterpreter interpreter = new XmlInterpreter(
+				new ManifestResourceSource(GetType(), "sample_config.xml"));
+			interpreter.Process(store);
+
+			Assert.AreEqual(2, store.GetFacilities().Length);
+			Assert.AreEqual(2, store.GetComponents().Length);
+
+			IConfiguration config = store.GetFacilityConfiguration("testidengine");
+			IConfiguration childItem = config.Children["item"];
+			Assert.IsNotNull(childItem);
+			Assert.AreEqual("value", childItem.Value);
+
+			config = store.GetFacilityConfiguration("testidengine2");
+			Assert.IsNotNull(config);
+			Assert.AreEqual("value within CDATA section", config.Value);
+
+			config = store.GetComponentConfiguration("testidcomponent1");
+			childItem = config.Children["item"];
+			Assert.IsNotNull(childItem);
+			Assert.AreEqual("value1", childItem.Value);
+
+			config = store.GetComponentConfiguration("testidcomponent2");
+			childItem = config.Children["item"];
+			Assert.IsNotNull(childItem);
+			Assert.AreEqual("value2", childItem.Value);;
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void MissingManifestResourceConifguration()
+		{
+			DefaultConfigurationStore store = new DefaultConfigurationStore();
+			XmlInterpreter interpreter = new XmlInterpreter(
+				new ManifestResourceSource(GetType(), "missing_config.xml"));
+			interpreter.Process(store);
 		}
 	}
 
---
+++ InversionOfControl/Castle.Windsor/Configuration/Sources/ManifestResourceSource.cs
@@ -0,0 +1,86 @@
+// Copyright 2004-2005 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.Windsor.Configuration.Sources
+{
+	using System;
+	using System.IO;
+	using System.Reflection;
+
+
+	public class ManifestResourceSource : IConfigurationSource
+	{
+		private StreamReader _reader;
+
+		public ManifestResourceSource(Type type, String resourceName)
+		{
+			if (type == null) throw new ArgumentNullException("true", "Null type");
+			if (resourceName == null || resourceName.Length == 0) throw new ArgumentException("resourceName", "Null or empty resource name");
+
+			ExtractResourceStream( type.Assembly, type, resourceName );
+		}
+
+		public ManifestResourceSource(Assembly assembly, String resourceName)
+		{
+			if (assembly == null) throw new ArgumentNullException("assembly", "Null assembly");
+			if (resourceName == null || resourceName.Length == 0) throw new ArgumentException("resourceName", "Null or empty resource name");
+
+			ExtractResourceStream( assembly, null, resourceName );
+		}
+
+		#region IConfigurationSource Members
+
+		public TextReader Contents
+		{
+			get
+			{
+				return _reader;
+			}
+		}
+
+		#endregion
+
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			if (_reader != null) _reader.Close();
+		}
+
+		#endregion
+
+		private void ExtractResourceStream(Assembly assembly, Type type, String resourceName)
+		{
+			Stream resourceStream = null;
+
+			if (type == null)
+			{
+				resourceStream = assembly.GetManifestResourceStream( resourceName );
+			}
+			else
+			{
+				resourceStream = assembly.GetManifestResourceStream( type, resourceName );
+			}
+
+			if (resourceStream == null)
+			{
+				if (type != null && type.Namespace != String.Empty)
+					resourceName = String.Format( "{0}.{1}", type.Namespace, resourceName );
+				throw new ArgumentException(String.Format( "Missing resource '{0}' in assembly {1}", resourceName, assembly.FullName ));
+			}
+
+			_reader = new StreamReader(resourceStream);
+		}
+	}
+}
