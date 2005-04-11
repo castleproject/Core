4dd5a150e61eacd3f288dfac111e24c705103f34
Henry Conceição <henry.conceicao@gmail.com>
2005-04-11 01:20:47 +0000

    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@427 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- CastleOnRails/TestSite/Controllers/AjaxController.cs
+++ CastleOnRails/TestSite/Controllers/AjaxController.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace TestSiteNVelocity.Controllers
+namespace TestSite.Controllers
 {
 	using System;
 	using System.Collections;
