7e57306283d4657650df12cb961d59863a0bfa43
Hamilton Verissimo <hammett@gmail.com>
2005-06-18 17:11:08 +0000

    Last few changes before "the big inner works change"
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@640 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- ActiveRecord/Castle.ActiveRecord.Tests/AbstractActiveRecordTest.cs
+++ ActiveRecord/Castle.ActiveRecord.Tests/AbstractActiveRecordTest.cs
@@ -26,17 +26,6 @@ namespace Castle.ActiveRecord.Tests
 		protected IConfigurationSource GetConfigSource()
 		{
 			return System.Configuration.ConfigurationSettings.GetConfig("activerecord") as IConfigurationSource;
-
-//			InPlaceConfigurationSource source = new InPlaceConfigurationSource();
-//			Hashtable properties = new Hashtable();
-//			properties.Add("hibernate.connection.driver_class", "NHibernate.Driver.SqlClientDriver");
-//			properties.Add("hibernate.dialect", "NHibernate.Dialect.MsSql2000Dialect");
-//			properties.Add("hibernate.connection.provider", "NHibernate.Connection.DriverConnectionProvider");
-//			properties.Add("hibernate.connection.connection_string", "UID=susa;Password=OverKkk;Initial Catalog=test;Data Source=.");
-//
-//			source.Add( typeof(ActiveRecordBase), properties );
-//
-//			return source;
 		}
 	}
 }
