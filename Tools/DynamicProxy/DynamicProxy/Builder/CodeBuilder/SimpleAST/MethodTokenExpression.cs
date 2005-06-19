61baa3179920f2e0a588982d7ee332bb67b4d2b3
Hamilton Verissimo <hammett@gmail.com>
2005-06-19 21:00:40 +0000

    Fix for Net 2.0
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@648 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Tools/DynamicProxy/DynamicProxy/Builder/CodeBuilder/SimpleAST/MethodTokenExpression.cs
+++ Tools/DynamicProxy/DynamicProxy/Builder/CodeBuilder/SimpleAST/MethodTokenExpression.cs
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 // Copyright 2004-2005 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -45,46 +44,3 @@ namespace Castle.DynamicProxy.Builder.CodeBuilder.SimpleAST
 		}
 	}
 }
-=======
-// Copyright 2004-2005 Castle Project - http://www.castleproject.org/
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-namespace Castle.DynamicProxy.Builder.CodeBuilder.SimpleAST
-{
-	using System;
-	using System.Reflection;
-	using System.Reflection.Emit;
-
-	/// <summary>
-	/// Summary description for MethodTokenExpression.
-	/// </summary>
-	public class MethodTokenExpression : Expression
-	{
-		private MethodInfo _method;
-
-		public MethodTokenExpression( MethodInfo method )
-		{
-			_method = method;
-		}
-
-		public override void Emit(IEasyMember member, ILGenerator gen)
-		{
-			gen.Emit(OpCodes.Ldtoken, _method);
-			gen.Emit(OpCodes.Call, typeof(MethodBase).GetMethod(
-				"GetMethodFromHandle", new Type[] { typeof(RuntimeMethodHandle) }));
-			gen.Emit(OpCodes.Castclass, typeof(MethodInfo));
-		}
-	}
-}
->>>>>>> .r637
