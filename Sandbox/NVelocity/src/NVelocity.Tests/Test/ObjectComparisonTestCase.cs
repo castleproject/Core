e8991e623ec86c3cfd569f34df8bddf4a7347682
Hamilton Verissimo <hammett@gmail.com>
2005-07-20 02:17:40 +0000

    Applied patch from CobraLord and setted eol to native
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@832 73e77b4c-caa6-f847-a29a-24ab75ae54b6

---
+++ Sandbox/NVelocity/src/NVelocity.Tests/Test/ObjectComparisonTestCase.cs
@@ -0,0 +1,198 @@
+// Copyright 2004-2005 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace NVelocity.Test
+{
+	using System;
+	using System.IO;
+	using NUnit.Framework;
+	using NVelocity.App;
+	using NVelocity.Runtime.Parser;
+	using NVelocity.Runtime.Parser.Node;
+
+	/// <summary>
+	/// Tests comparison of custom objects.
+	/// </summary>
+	[TestFixture]
+	public class ObjectComparisonTestCase
+	{
+		#region Constants
+
+		private const string VmTemplate	= @"#if ($left == $right )equal
+#else
+different
+#end
+#if ($left > $right)
+greater
+#end
+#if ($left < $right)
+smaller
+#end
+#if ($left >= $right)
+geq
+#end
+#if ($left <= $right)
+leq
+#end";
+
+		private const string CmpEqual = @"equal
+geq
+leq
+";
+		private const string CmpGreater = @"different
+greater
+geq
+";
+		private const string CmpSmaller = @"different
+smaller
+leq
+";
+
+		#endregion
+
+		private int Int			= 11;
+		private ulong ULong		= 12UL;
+		private float Float		= 13.878F;
+		private double Double	= 15.059D;
+		
+		private VelocityEngine ve;
+		private VelocityContext ctx;
+			
+		[TestFixtureSetUp]
+		public void SetUp()
+		{
+			ve = new VelocityEngine();
+			ve.Init();
+
+			ctx = new VelocityContext();
+		}
+		[Test]
+		public void ComparePrimitive()
+		{
+			Assert.AreEqual( ObjectComparer.Equal, ObjectComparer.CompareObjects( Int, Int ) );
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( Int, Double ) );
+			Assert.AreEqual( ObjectComparer.Greater, ObjectComparer.CompareObjects( Double, Int ) );
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( Float, Double ) );
+			Assert.AreEqual( ObjectComparer.Greater, ObjectComparer.CompareObjects( Double, Float ) );
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( Int, Float ) );
+			Assert.AreEqual( ObjectComparer.Greater, ObjectComparer.CompareObjects( Float, Int ) );
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( ULong, Float ) );
+			Assert.AreEqual( ObjectComparer.Greater, ObjectComparer.CompareObjects( Double, ULong ) );
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( Int, ULong ) );
+
+			VmCompareCouple( Int, Double );
+			VmCompareCouple( Int, Float );
+			VmCompareCouple( Int, ULong );
+			VmCompareCouple( ULong, Float );
+			VmCompareCouple( Float, Double );
+			VmCompareCouple( ULong, Double );
+		}
+
+		/// <summary>
+		/// String does an alphabetical comparision, if you compare an object to a string, the
+		/// string value of that object will be used.  Char does an ascii comparison.
+		/// </summary>
+		[Test]
+		public void CompareString()
+		{
+			string aaa		= "aaa";
+			string bbb		= "aab";
+
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( aaa, bbb ) );
+			Assert.AreEqual( ObjectComparer.Greater	, ObjectComparer.CompareObjects( bbb, aaa ) );
+			Assert.AreEqual( ObjectComparer.Equal, ObjectComparer.CompareObjects( aaa, aaa ) );
+
+			VmCompareCouple( aaa, bbb );
+
+			char c			= 'c';
+			short s			= 7;
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( bbb, c ) );
+			Assert.AreEqual( ObjectComparer.Greater	, ObjectComparer.CompareObjects( c, bbb ) );
+			Assert.AreEqual( ObjectComparer.Equal, ObjectComparer.CompareObjects( c, c ) );
+
+			VmCompareCouple( aaa, c );
+			VmCompareCouple( bbb, c );
+
+			Assert.AreEqual( ObjectComparer.Greater, ObjectComparer.CompareObjects( aaa, s ) );
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( s, bbb ) );
+
+			Assert.AreEqual( ObjectComparer.Greater, ObjectComparer.CompareObjects( c, s ) );
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( s, c ) );
+
+			Assert.AreEqual( ObjectComparer.Greater, ObjectComparer.CompareObjects( c, aaa ) );
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( bbb, c ) );
+
+			VmCompareCouple( s, aaa );
+			VmCompareCouple( s, c );
+		}
+
+		[Test]
+		public void CompareDateTime()
+		{
+			DateTime now	= DateTime.Now;
+			DateTime next	= now.AddSeconds( 3 );
+
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( now, next ) );
+			Assert.AreEqual( ObjectComparer.Greater, ObjectComparer.CompareObjects( next, now ) );
+			Assert.AreEqual( ObjectComparer.Equal, ObjectComparer.CompareObjects( now, now ) );
+
+			VmCompareCouple( now, next );
+		}
+
+		[Test]
+		public void CompareTimeSpan()
+		{
+			TimeSpan longer	= new TimeSpan( 15, 12, 37, 12 );
+			TimeSpan shorter = new TimeSpan( 17, 56, 59 );
+
+			Assert.AreEqual( ObjectComparer.Greater, ObjectComparer.CompareObjects( longer, shorter ) );
+			Assert.AreEqual( ObjectComparer.Smaller, ObjectComparer.CompareObjects( shorter, longer ) );
+			Assert.AreEqual( ObjectComparer.Equal, ObjectComparer.CompareObjects( shorter, shorter ) );
+
+			VmCompareCouple( shorter, longer );
+		}
+
+		public void VmCompareCouple( object small, object big )
+		{
+			ctx.Put( "left", small );
+			ctx.Put( "right", small );
+
+			StringWriter sw = new StringWriter();
+			Assert.IsTrue( ve.Evaluate( ctx, sw, string.Empty, VmTemplate ) );
+			Assert.AreEqual( CmpEqual, sw.ToString() );
+			
+			ctx.Put( "left", big );
+			ctx.Put( "right", big );
+
+			sw = new StringWriter();
+			Assert.IsTrue( ve.Evaluate( ctx, sw, string.Empty, VmTemplate ) );
+			Assert.AreEqual( CmpEqual, sw.ToString() );
+			
+			ctx.Put( "left", small );
+			ctx.Put( "right", big );
+
+			sw = new StringWriter();
+			Assert.IsTrue( ve.Evaluate( ctx, sw, string.Empty, VmTemplate ) );
+			Assert.AreEqual( CmpSmaller, sw.ToString() );
+
+			ctx.Put( "left", big );
+			ctx.Put( "right", small );
+
+			sw = new StringWriter();
+			Assert.IsTrue( ve.Evaluate( ctx, sw, string.Empty, VmTemplate ) );
+			Assert.AreEqual( CmpGreater, sw.ToString() );
+
+		}
+	}
+}
--- Sandbox/NVelocity/src/NVelocity/Runtime/Parser/Node/ASTNENode.cs
+++ Sandbox/NVelocity/src/NVelocity/Runtime/Parser/Node/ASTNENode.cs
@@ -1,58 +1,19 @@
+// Copyright 2004-2005 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 namespace NVelocity.Runtime.Parser.Node
 {
-	/*
-    * The Apache Software License, Version 1.1
-    *
-    * Copyright (c) 2000-2001 The Apache Software Foundation.  All rights
-    * reserved.
-    *
-    * Redistribution and use in source and binary forms, with or without
-    * modification, are permitted provided that the following conditions
-    * are met:
-    *
-    * 1. Redistributions of source code must retain the above copyright
-    *    notice, this list of conditions and the following disclaimer.
-    *
-    * 2. Redistributions in binary form must reproduce the above copyright
-    *    notice, this list of conditions and the following disclaimer in
-    *    the documentation and/or other materials provided with the
-    *    distribution.
-    *
-    * 3. The end-user documentation included with the redistribution, if
-    *    any, must include the following acknowlegement:
-    *       "This product includes software developed by the
-    *        Apache Software Foundation (http://www.apache.org/)."
-    *    Alternately, this acknowlegement may appear in the software itself,
-    *    if and wherever such third-party acknowlegements normally appear.
-    *
-    * 4. The names "The Jakarta Project", "Velocity", and "Apache Software
-    *    Foundation" must not be used to endorse or promote products derived
-    *    from this software without prior written permission. For written
-    *    permission, please contact apache@apache.org.
-    *
-    * 5. Products derived from this software may not be called "Apache"
-    *    nor may "Apache" appear in their names without prior written
-    *    permission of the Apache Group.
-    *
-    * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
-    * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-    * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
-    * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-    * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-    * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-    * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-    * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-    * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-    * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-    * SUCH DAMAGE.
-    * ====================================================================
-    *
-    * This software consists of voluntary contributions made by many
-    * individuals on behalf of the Apache Software Foundation.  For more
-    * information on the Apache Software Foundation, please see
-    * <http://www.apache.org/>.
-    */
 	using System;
 	using NVelocity.Context;
 
@@ -78,34 +39,18 @@ namespace NVelocity.Runtime.Parser.Node
 			Object left = jjtGetChild(0).Value(context);
 			Object right = jjtGetChild(1).Value(context);
 
-			/*
-	    *  null check
-	    */
-
-			if (left == null || right == null)
+			try
 			{
-				rsvc.error((left == null ? "Left" : "Right") + " side (" + jjtGetChild((left == null ? 0 : 1)).literal() + ") of '!=' operation has null value." + " Operation not possible. " + context.CurrentTemplateName + " [line " + Line + ", column " + Column + "]");
-				return false;
-
+				return ObjectComparer.CompareObjects( left, right ) != 0;
+			}
+			catch
+			{
+				// Ignore, we can't compare decently by value, but we honestly don't give a sh*t
 			}
 
-
-			/*
-	    *  check to see if they are the same class.  I don't think this is slower
-	    *  as I don't think that getClass() results in object creation, and we can
-	    *  extend == to handle all classes
-	    */
-
-			if (left.GetType().Equals(right.GetType()))
-			{
-				return !(left.Equals(right));
-			}
-			else
-			{
-				rsvc.error("Error in evaluation of != expression." + " Both arguments must be of the same Class." + " Currently left = " + left.GetType() + ", right = " + right.GetType() + ". " + context.CurrentTemplateName + " [line " + Line + ", column " + Column + "] (ASTEQNode)");
-
-				return false;
-			}
+			// If we can't actually compare the objects, try the operator as fallback
+			// For operator overloaded types, this will not really be a reference comp, but that's ok.
+			return left != right;
 		}
 	}
 }
