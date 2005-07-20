bd3687801459ad8fb23088e084aca874f75d4d5b
Dru Sellers <dru@drusellers.com>
2005-07-20 13:52:47 +0000

    much better logging testing. thanks Roelof.
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@848 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Facilities/Logging/Castle.Facilities.Logging.Tests/LoggingFacilityTestCase.cs
+++ Facilities/Logging/Castle.Facilities.Logging.Tests/LoggingFacilityTestCase.cs
@@ -15,53 +15,74 @@
 namespace Castle.Facilities.Logging.Tests
 {
     using System;
+    using System.IO;
     using Castle.MicroKernel.SubSystems.Configuration;
     using Castle.Model.Configuration;
     using Castle.Windsor;
     using NUnit.Framework;
-
-    using Castle.MicroKernel;
-
 
 	/// <summary>
 	/// Summary description for LoggingFacilityTestCase.
 	/// </summary>
     [TestFixture]
     public class LoggingFacilityTestCase {
-        private IWindsorContainer kernel;
+        private IWindsorContainer log4netContainer;
+        private IWindsorContainer nlogContainer;
+        private StringWriter outWriter = new StringWriter();
+        private StringWriter errorWriter = new StringWriter();
 
         [SetUp]
         public void Setup()
         {
-            kernel = CreateConfiguredContainer();
-            kernel.AddFacility("logging", new LoggingFacility());
+            log4netContainer = CreateConfiguredContainerForlog4net();
+            log4netContainer.AddFacility("logging", new LoggingFacility());
+            nlogContainer = CreateConfiguredContainerForNLog();
+            nlogContainer.AddFacility("logging", new LoggingFacility());
+
+            outWriter.GetStringBuilder().Length = 0;
+            errorWriter.GetStringBuilder().Length = 0;
+
+            Console.SetOut(outWriter);
+            Console.SetError(errorWriter);
         }
 
         [TearDown]
         public void Teardown()
         {
-            kernel.Dispose();
-            kernel = null;
+            log4netContainer.Dispose();
+            nlogContainer.Dispose();
         }
 
         [Test]
-        public void SimpleTest() {
-            String expectedLogOutput = "HelloWorld";
+        public void Simplelog4netTest() {
+            String expectedLogOutput = String.Format("INFO  {0} - Hello world\r\n", typeof(Classes.LoggingComponent).ToString());
             String actualLogOutput = "";
 
-            kernel.AddComponent("component", typeof(Classes.LoggingComponent));
+            log4netContainer.AddComponent("component", typeof(Classes.LoggingComponent));
+            Classes.LoggingComponent test = log4netContainer["component"] as Classes.LoggingComponent;
 
-            Classes.LoggingComponent test = kernel["component"] as Classes.LoggingComponent;
+            test.HelloWorld();
 
-            test.HelloWorld(); //should cause some kind of HelloWorld to be logged.
-
-            
-            //dump log output to the actualLogOutput variable
-
-            Assert.IsTrue(expectedLogOutput.Equals(actualLogOutput));
+            actualLogOutput = outWriter.GetStringBuilder().ToString();
+            Assert.AreEqual(expectedLogOutput, actualLogOutput);
         }
 
-        protected virtual IWindsorContainer CreateConfiguredContainer()
+        [Test]
+        public void SimpleNLogTest() 
+        {
+            String expectedLogOutput = String.Format("INFO  {0} - Hello world\r\n", typeof(Classes.LoggingComponent).ToString());
+            String actualLogOutput = "";
+
+            nlogContainer.AddComponent("component", typeof(Classes.LoggingComponent));
+            Classes.LoggingComponent test = nlogContainer["component"] as Classes.LoggingComponent;
+
+            test.HelloWorld();
+
+            actualLogOutput = outWriter.GetStringBuilder().ToString();
+            Assert.AreEqual(expectedLogOutput, actualLogOutput);
+        }
+
+        protected virtual IWindsorContainer CreateConfiguredContainerForlog4net()
         {
             IWindsorContainer container = new WindsorContainer(new DefaultConfigurationStore());
 
@@ -69,8 +90,22 @@ namespace Castle.Facilities.Logging.Tests
 
             confignode.Children.Add(new MutableConfiguration("framework", "log4net"));
             confignode.Children.Add(new MutableConfiguration("config", "log4net.config"));
-            confignode.Children.Add(new MutableConfiguration("interception", "false"));
-            
+            confignode.Children.Add(new MutableConfiguration("interception", "false"));            
+
+            container.Kernel.ConfigurationStore.AddFacilityConfiguration("logging", confignode);
+
+            return container;
+        }
+
+        protected virtual IWindsorContainer CreateConfiguredContainerForNLog()
+        {
+            IWindsorContainer container = new WindsorContainer(new DefaultConfigurationStore());
+
+            MutableConfiguration confignode = new MutableConfiguration("facility");
+
+            confignode.Children.Add(new MutableConfiguration("framework", "nlog"));
+            confignode.Children.Add(new MutableConfiguration("config", "nlog.config"));
+            confignode.Children.Add(new MutableConfiguration("interception", "false"));            
 
             container.Kernel.ConfigurationStore.AddFacilityConfiguration("logging", confignode);
 
