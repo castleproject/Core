068b7b2d4fad19c94d4be58acb68757a7eeddf31
Hamilton Verissimo <hammett@gmail.com>
2005-09-09 01:51:22 +0000

    Adding first EnterpriseLibrary integration: configuration
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@984 73e77b4c-caa6-f847-a29a-24ab75ae54b6

---
+++ Facilities/EnterpriseLibrary/Castle.Facilities.EnterpriseLibrary.Configuration/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]		
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+#if STRONG
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("../../CastleKey.snk")]
+// [assembly: AssemblyKeyFile(@"E:\dev\projects\castle\CastleKey.snk")]
+[assembly: AssemblyKeyName("")]
+#endif
+
