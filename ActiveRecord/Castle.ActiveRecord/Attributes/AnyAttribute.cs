4142a617aee098d795e68a2d4441aa43f7c2177d
Hamilton Verissimo <hammett@gmail.com>
2005-10-10 13:57:57 +0000

    Header and style changes
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@1124 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- ActiveRecord/Castle.ActiveRecord/Attributes/AnyAttribute.cs
+++ ActiveRecord/Castle.ActiveRecord/Attributes/AnyAttribute.cs
@@ -1,22 +1,35 @@
-using System;
+// Copyright 2004-2005 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 namespace Castle.ActiveRecord
 {
+	using System;
+
 	[AttributeUsage(AttributeTargets.Property, AllowMultiple=false), Serializable]
 	public class AnyAttribute : WithAccessAttribute
 	{		
+		private CascadeEnum cascade;
+		private Type idType;
+		private Type metaType;
+		private string typeColumn, idColumn;
+		private string index;
+		private bool insert = true, update = true;
+
 		public AnyAttribute(Type idType)
 		{
 			this.idType = idType;
 		}
-
-		private CascadeEnum cascade;
-		private string typeColumn, idColumn;
-		private Type idType;
-		private Type metaType;
-		private string index;
-		private bool insert = true, 
-			update = true;
 
 		public Type IdType
 		{
@@ -67,7 +80,9 @@ namespace Castle.ActiveRecord
 		}
 	}
 
-	//Avoids the AnyAttribute.MetaValue problem
+	/// <summary>
+	/// Avoids the AnyAttribute.MetaValue problem
+	/// </summary>
 	public class Any
 	{
 		[AttributeUsage(AttributeTargets.Property, AllowMultiple=true), Serializable]
@@ -81,7 +96,6 @@ namespace Castle.ActiveRecord
 				this._value = value;
 				this._clazz = clazz;
 			}
-
 
 			public string Value
 			{
