42f92cc26210018fac0c635f9dd99ae5088d64f9
Hamilton Verissimo <hammett@gmail.com>
2005-11-26 00:49:53 +0000

    Small refactoring
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@1377 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- MonoRail/Castle.MonoRail.Framework/Extensions/ExceptionChaining/AbstractExceptionHandler.cs
+++ MonoRail/Castle.MonoRail.Framework/Extensions/ExceptionChaining/AbstractExceptionHandler.cs
@@ -15,7 +15,13 @@
 namespace Castle.MonoRail.Framework.Extensions.ExceptionChaining
 {
 	using System;
+	using System.Collections;
+	using System.Collections.Specialized;
+	using System.Text;
 
+	/// <summary>
+	/// Provides a basic implementation of <see cref="IExceptionHandler"/>
+	/// </summary>
 	public abstract class AbstractExceptionHandler : IExceptionHandler
 	{
 		private IExceptionHandler nextHandler;
@@ -38,6 +44,95 @@ namespace Castle.MonoRail.Framework.Extensions.ExceptionChaining
 			{
 				nextHandler.Process(context, serviceProvider);
 			}
+		}
+
+		protected string BuildStandardMessage(IRailsEngineContext context)
+		{
+			StringBuilder sbMessage = new StringBuilder();
+	
+			sbMessage.Append("Controller details\r\n");
+			sbMessage.Append("==================\r\n\r\n");
+			sbMessage.AppendFormat("Url {0}\r\n", context.Url);
+			sbMessage.AppendFormat("Area {0}\r\n", context.UrlInfo.Area);
+			sbMessage.AppendFormat("Controller {0}\r\n", context.UrlInfo.Controller);
+			sbMessage.AppendFormat("Action {0}\r\n", context.UrlInfo.Action);
+			sbMessage.AppendFormat("Extension {0}\r\n\r\n", context.UrlInfo.Extension);
+	
+			sbMessage.Append("Exception details\r\n");
+			sbMessage.Append("=================\r\n\r\n");
+			sbMessage.AppendFormat("Exception occured at {0}\r\n", DateTime.Now);
+			RecursiveDumpException(context.LastException, sbMessage, 0);
+	
+			sbMessage.Append("\r\nEnvironment and params");
+			sbMessage.Append("\r\n======================\r\n\r\n");
+			sbMessage.AppendFormat("ApplicationPath {0}\r\n", context.ApplicationPath);
+			sbMessage.AppendFormat("ApplicationPhysicalPath {0}\r\n", context.ApplicationPhysicalPath);
+			sbMessage.AppendFormat("RequestType {0}\r\n", context.RequestType);
+			sbMessage.AppendFormat("UrlReferrer {0}\r\n", context.UrlReferrer);
+	
+			if (context.CurrentUser != null)
+			{
+				sbMessage.AppendFormat("CurrentUser.Name {0}\r\n", context.CurrentUser.Identity.Name);
+				sbMessage.AppendFormat("CurrentUser.AuthenticationType {0}\r\n", context.CurrentUser.Identity.AuthenticationType);
+				sbMessage.AppendFormat("CurrentUser.IsAuthenticated {0}\r\n", context.CurrentUser.Identity.IsAuthenticated);
+			}
+	
+			DumpDictionary(context.Flash, "Flash", sbMessage);
+	
+			DumpDictionary(context.Params, "Params", sbMessage);
+	
+			DumpDictionary(context.Session, "Session", sbMessage);
+
+			return sbMessage.ToString();
+		}
+
+		private void DumpDictionary(IDictionary dict, String title, StringBuilder message)
+		{
+			message.AppendFormat("{0}: \r\n", title);
+
+			try
+			{
+				foreach(DictionaryEntry entry in dict)
+				{
+					message.AppendFormat("\r\n\t{0}:{1}", entry.Key, entry.Value);
+				}
+			}
+			catch(Exception)
+			{
+				// Ignores
+			}
+		}
+
+		private void DumpDictionary(NameValueCollection dict, String title, StringBuilder message)
+		{
+			message.AppendFormat("{0}: \r\n", title);
+
+			try
+			{
+				foreach(String key in dict.Keys)
+				{
+					message.AppendFormat("\r\n\t{0}:{1} \r\n", key, dict[key]);
+				}
+			}
+			catch(Exception)
+			{
+				// Ignores
+			}
+		}
+
+		private void RecursiveDumpException(Exception exception, StringBuilder message, int nested)
+		{
+			if (exception == null) return;
+
+			char[] spaceBuff = new char[nested * 2]; 
+			for(int i = 0; i < nested * 2; i++) spaceBuff[i] = ' ';
+
+			String space = new String(spaceBuff);
+
+			message.AppendFormat("{0}Exception: {1}\r\n", space, exception.Message);
+			message.AppendFormat("{0}Stack Trace:\r\n{0}{1}\r\n", space, exception.StackTrace);
+
+			RecursiveDumpException(exception.InnerException, message, nested + 1);
 		}
 	}
 }
--- MonoRail/Castle.MonoRail.Framework/Extensions/ExceptionChaining/EmailHandler.cs
+++ MonoRail/Castle.MonoRail.Framework/Extensions/ExceptionChaining/EmailHandler.cs
@@ -15,10 +15,7 @@
 namespace Castle.MonoRail.Framework.Extensions.ExceptionChaining
 {
 	using System;
-	using System.Collections;
-	using System.Collections.Specialized;
 	using System.Configuration;
-	using System.Text;
 	using System.Xml;
 
 	using Castle.Components.Common.EmailSender;
@@ -55,49 +52,12 @@ namespace Castle.MonoRail.Framework.Extensions.ExceptionChaining
 		{
 			IEmailSender emailSender = (IEmailSender) serviceProvider.GetService( typeof(IEmailSender) );
 
-			StringBuilder sbMessage = new StringBuilder();
-
-			sbMessage.Append("Hello, this message was sent by EmailHandler (part of MonoRail framework) ");
-			sbMessage.Append("which was enabled possibly by you. The following details an unexpected exception ");
-			sbMessage.Append("that happened during the process of an action. It might or might not be handled by  ");
-			sbMessage.Append("a rescue.\r\n\r\n");
-
-			sbMessage.Append("Controller details\r\n");
-			sbMessage.Append("==================\r\n\r\n");
-			sbMessage.AppendFormat("Url {0}\r\n", context.Url);
-			sbMessage.AppendFormat("Area {0}\r\n", context.UrlInfo.Area);
-			sbMessage.AppendFormat("Controller {0}\r\n", context.UrlInfo.Controller);
-			sbMessage.AppendFormat("Action {0}\r\n", context.UrlInfo.Action);
-			sbMessage.AppendFormat("Extension {0}\r\n\r\n", context.UrlInfo.Extension);
-
-			sbMessage.Append("Exception details\r\n");
-			sbMessage.Append("=================\r\n\r\n");
-			sbMessage.AppendFormat("Exception occured at {0}\r\n", DateTime.Now);
-			RecursiveDumpException(context.LastException, sbMessage, 0);
-
-			sbMessage.Append("\r\nEnvironment and params\r\n");
-			sbMessage.Append("======================\r\n\r\n");
-			sbMessage.AppendFormat("ApplicationPath {0}\r\n", context.ApplicationPath);
-			sbMessage.AppendFormat("ApplicationPhysicalPath {0}\r\n", context.ApplicationPhysicalPath);
-			sbMessage.AppendFormat("RequestType {0}\r\n", context.RequestType);
-			sbMessage.AppendFormat("UrlReferrer {0}\r\n", context.UrlReferrer);
-
-			if (context.CurrentUser != null)
-			{
-				sbMessage.AppendFormat("CurrentUser.Name {0}\r\n", context.CurrentUser.Identity.Name);
-				sbMessage.AppendFormat("CurrentUser.AuthenticationType {0}\r\n", context.CurrentUser.Identity.AuthenticationType);
-				sbMessage.AppendFormat("CurrentUser.IsAuthenticated {0}\r\n", context.CurrentUser.Identity.IsAuthenticated);
-			}
-
-			DumpDictionary(context.Flash, "Flash", sbMessage);
-
-			DumpDictionary(context.Params, "Params", sbMessage);
-
-			DumpDictionary(context.Session, "Session", sbMessage);
+			String message = BuildStandardMessage(context);
 
 			try
 			{
-				emailSender.Send( mailFrom, mailTo, "MonoRail Exception", sbMessage.ToString() );
+				emailSender.Send( mailFrom, mailTo, 
+					"MonoRail Exception: " + context.LastException.GetType().Name, message );
 			}
 			catch(Exception)
 			{
@@ -105,55 +65,6 @@ namespace Castle.MonoRail.Framework.Extensions.ExceptionChaining
 			}
 
 			InvokeNext(context, serviceProvider);
-		}
-
-		private void DumpDictionary(IDictionary dict, String title, StringBuilder message)
-		{
-			message.AppendFormat("{0}: \r\n", title);
-
-			try
-			{
-				foreach(DictionaryEntry entry in dict)
-				{
-					message.AppendFormat("    {0}:{1} \r\n", entry.Key, entry.Value);
-				}
-			}
-			catch(Exception)
-			{
-				// Ignores
-			}
-		}
-
-		private void DumpDictionary(NameValueCollection dict, String title, StringBuilder message)
-		{
-			message.AppendFormat("{0}: \r\n", title);
-
-			try
-			{
-				foreach(String key in dict.Keys)
-				{
-					message.AppendFormat("    {0}:{1} \r\n", key, dict[key]);
-				}
-			}
-			catch(Exception)
-			{
-				// Ignores
-			}
-		}
-
-		private void RecursiveDumpException(Exception exception, StringBuilder message, int nested)
-		{
-			if (exception == null) return;
-
-			char[] spaceBuff = new char[nested * 2]; 
-			for(int i = 0; i < nested * 2; i++) spaceBuff[i] = ' ';
-
-			String space = new String(spaceBuff);
-
-			message.AppendFormat("{0}Exception: {1}\r\n", space, exception.Message);
-			message.AppendFormat("{0}Stack Trace:\r\n{1}\r\n\r\n", space, exception.StackTrace);
-
-			RecursiveDumpException(exception.InnerException, message, nested + 1);
 		}
 	}
 }
