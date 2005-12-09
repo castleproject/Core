27515780d0fb764010b1f08e5a5f2148d34291ac
Hamilton Verissimo <hammett@gmail.com>
2005-12-09 02:58:20 +0000

    Documentation improved
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@1446 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- ActiveRecord/Castle.ActiveRecord/Framework/Scopes/ISessionScope.cs
+++ ActiveRecord/Castle.ActiveRecord/Framework/Scopes/ISessionScope.cs
@@ -34,32 +34,46 @@ namespace Castle.ActiveRecord
 	/// AR for the scope lifetime. Session cache and
 	/// transaction are the best examples, but you 
 	/// can create new scopes adding new semantics.
+	/// <para>
+	/// The methods on this interface are mostly invoked
+	/// by the <see cref="Castle.ActiveRecord.Framework.ISessionFactoryHolder"/>
+	/// implementation
+	/// </para>
 	/// </remarks>
 	public interface ISessionScope : IDisposable
 	{
 		SessionScopeType ScopeType { get; }
 
 		/// <summary>
-		/// Implementors should return true 
-		/// if the key is know. The key might be whatever the
-		/// scope implementation wants
+		/// This method is invoked when no session was available
+		/// at and the <see cref="Castle.ActiveRecord.Framework.ISessionFactoryHolder"/>
+		/// just created one. So it registers the session created 
+		/// within this scope using a key. The scope implementation
+		/// shouldn't make any assumption on what the key
+		/// actually is as we reserve the right to change it 
+		/// <seealso cref="IsKeyKnown"/>
 		/// </summary>
-		/// <param name="key"></param>
-		/// <returns></returns>
-		bool IsKeyKnown(object key);
-
-		/// <summary>
-		/// Associates a session with this scope
-		/// </summary>
-		/// <param name="key"></param>
-		/// <param name="session"></param>
+		/// <param name="key">an object instance</param>
+		/// <param name="session">An instance of <c>ISession</c></param>
 		void RegisterSession(object key, ISession session);
 
 		/// <summary>
-		/// Returns the session associated with the key
+		/// This method is invoked when the 
+		/// <see cref="Castle.ActiveRecord.Framework.ISessionFactoryHolder"/>
+		/// instance needs a session instance. Instead of creating one it interrogates
+		/// the active scope for one. The scope implementation must check if it
+		/// has a session registered for the given key. 
+		/// <seealso cref="RegisterSession"/>
 		/// </summary>
-		/// <param name="key"></param>
-		/// <returns></returns>
+		/// <param name="key">an object instance</param>
+		/// <returns><c>true</c> if the key exists within this scope instance</returns>
+		bool IsKeyKnown(object key);
+
+		/// <summary>
+		/// This method should return the session instance associated with the key.
+		/// </summary>
+		/// <param name="key">an object instance</param>
+		/// <returns>the session instance or null if none was found</returns>
 		ISession GetSession(object key);
 	}
 }
