8d0d90e88e82a2887f9c6c4dbd6171cebfd7fa25
Hamilton Verissimo <hammett@gmail.com>
2005-12-13 01:41:33 +0000

    Pestcontrol fix
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@1470 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Samples/PestControl/Castle.Applications.PestControl.Web/PestControlApplication.cs
+++ Samples/PestControl/Castle.Applications.PestControl.Web/PestControlApplication.cs
@@ -15,17 +15,18 @@
 namespace Castle.Applications.PestControl.Web
 {
 	using System;
-	using System.Web;
 	using System.Web.Security;
+	using System.Web;
+
+	using Castle.Applications.PestControl.Web.Controllers.Filters;
+	using Castle.Model.Resource;
+	using Castle.Windsor;
+	using Castle.Windsor.Configuration.Interpreters;
+
+	using Castle.MonoRail.WindsorExtension;
 
 	using Castle.Applications.PestControl.Model;
 	using Castle.Applications.PestControl.Web.Controllers;
-	using Castle.Applications.PestControl.Web.Controllers.Filters;
-	using Castle.MicroKernel.SubSystems.Configuration;
-	using Castle.Model.Resource;
-	using Castle.MonoRail.WindsorExtension;
-	using Castle.Windsor;
-	using Castle.Windsor.Configuration.Interpreters;
 
 	/// <summary>
 	/// Summary description for PestControlApplication.
@@ -34,38 +35,47 @@ namespace Castle.Applications.PestControl.Web
 	{
 		private static WindsorContainer container;
 
+		public void Application_OnStart() 
 		{
-			DefaultConfigurationStore store = new DefaultConfigurationStore();
-			XmlInterpreter interpreter = new XmlInterpreter(new ConfigResource());
-			interpreter.ProcessResource(interpreter.Source, store);
-			container = new PestControlContainer(interpreter);
+			container = new PestControlContainer( new XmlInterpreter(new ConfigResource()) );
 
-			container.AddFacility("rails", new RailsFacility());
+			container.AddFacility( "rails", new RailsFacility() );
 
-			this.AddFiltersAndControllers(container);
+			AddFiltersAndControllers(container);
 		}
 
 		private void AddFiltersAndControllers(WindsorContainer container)
 		{
+			container.AddComponent( "auth.filter", typeof(CheckAuthenticationFilter) );
+			container.AddComponent( "home", typeof(HomeController) );
+			container.AddComponent( "dashboard", typeof(DashboardController) );
+			container.AddComponent( "registration", typeof(RegistrationController) );
+			container.AddComponent( "project", typeof(ProjectController) );
 		}
 
+		public void Application_OnEnd() 
 		{
 			container.Dispose();
 		}
 
 		public void FormsAuthentication_OnAuthenticate(Object sender, FormsAuthenticationEventArgs e)
 		{
+			HttpCookie cookie = e.Context.Request.Cookies[ FormsAuthentication.FormsCookieName ];
 
+			if (cookie == null) return;
 
 			FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(cookie.Value);
 
+			PestControlModel model = (PestControlModel) container[ typeof(PestControlModel) ];
 
+			e.User = model.Users.FindByEmail( ticket.Name );
 		}
 
 		#region IContainerAccessor implementation
 
 		public IWindsorContainer Container
 		{
+			get { return container; }
 		}
 
 		#endregion
