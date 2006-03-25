b9e3854bca798e20904c8b938ebd3e186293fa69
Dru Sellers <dru@drusellers.com>
2006-03-25 04:16:58 +0000

    I refactored the code into smaller methods. Exception handling has been changed in a minor way (changed from throw ex; => throw;) to preserve the call stack. The exception types should be the same just more information.
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@1783 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Tools/NVelocity/src/NVelocity/Runtime/Directive/Parse.cs
+++ Tools/NVelocity/src/NVelocity/Runtime/Directive/Parse.cs
@@ -54,43 +54,21 @@ namespace NVelocity.Runtime.Directive
 		public override bool Render(IInternalContextAdapter context, TextWriter writer, Node node)
 		{
 			// did we get an argument?
-			if (node.GetChild(0) == null)
-			{
-				rsvc.Error("#parse() error :  null argument");
-				return false;
-			}
+            if(!this.AssertArgument(node)) return false;
 
 			// does it have a value?  If you have a null reference, then no.
-			Object value_ = node.GetChild(0).Value(context);
-
-			if (value_ == null)
-			{
-				rsvc.Error("#parse() error :  null argument");
-				return false;
-			}
+            Object value_;
+            if(!this.AssertNodeHasValue(node, context, out value_)) return false;
 
 			// get the path
 			String arg = value_.ToString();
 
-			// see if we have exceeded the configured depth.
-	    // If it isn't configured, put a stop at 20 just in case.
-			Object[] templateStack = context.TemplateNameStack;
-
-			if (templateStack.Length >= rsvc.GetInt(RuntimeConstants.PARSE_DIRECTIVE_MAXDEPTH, 20))
-			{
-				StringBuilder path = new StringBuilder();
-
-				for (int i = 0; i < templateStack.Length; ++i)
-					path.Append(" > " + templateStack[i]);
-
-				rsvc.Error("Max recursion depth reached (" + templateStack.Length + ")" + " File stack:" + path);
-				return false;
-			}
+            AssertTemplateStack(context);
 
 			Resource current = context.CurrentResource;
 
 			// get the resource, and assume that we use the encoding of the current template
-	    // the 'current resource' can be null if we are processing a stream....
+	        // the 'current resource' can be null if we are processing a stream....
 			String encoding = null;
 
 			if (current != null)
@@ -101,51 +79,114 @@ namespace NVelocity.Runtime.Directive
 			// now use the Runtime resource loader to get the template
 			Template t = null;
 
-			try
-			{
-				t = rsvc.GetTemplate(arg, encoding);
-			}
-			catch (ResourceNotFoundException rnfe)
-			{
-				// the arg wasn't found.  Note it and throw
-				rsvc.Error("#parse(): cannot find template '" + arg + "', called from template " + context.CurrentTemplateName + " at (" + Line + ", " + Column + ")");
-				throw rnfe;
-			}
-			catch (ParseErrorException pee)
-			{
-				// the arg was found, but didn't parse - syntax error
-				// note it and throw
-				rsvc.Error("#parse(): syntax error in #parse()-ed template '" + arg + "', called from template " + context.CurrentTemplateName + " at (" + Line + ", " + Column + ")");
-
-				throw pee;
-			}
-			catch (Exception e)
-			{
-				rsvc.Error("#parse() : arg = " + arg + ".  Exception : " + e);
-				return false;
-			}
+            t = this.GetTemplate(arg, encoding, context);
+            if(t == null) return false;
 
 			// and render it
-			try
-			{
-				context.PushCurrentTemplateName(arg);
-				((SimpleNode) t.Data).Render(context, writer);
-			}
-			catch (Exception e)
-			{
-				// if it's a MIE, it came from the render.... throw it...
-				if (e is MethodInvocationException)
-					throw;
-
-				rsvc.Error("Exception rendering #parse( " + arg + " )  : " + e);
-				return false;
-			}
-			finally
-			{
-				context.PopCurrentTemplateName();
-			}
+            if(!this.RenderTemplate(t, arg, writer, context)) return false;
 
 			return true;
 		}
+
+        private bool AssertArgument(Node node)
+        {
+            bool result = true;
+            if(node.GetChild(0) == null)
+            {
+                rsvc.Error("#parse() error :  null argument");
+                result = false;
+            }
+            return result;
+        }
+        private bool AssertNodeHasValue(Node node, IInternalContextAdapter context, out Object value)
+        {
+            bool result = true;
+
+            value = node.GetChild(0).Value(context);
+            
+            if(value == null)
+            {
+                rsvc.Error("#parse() error :  null argument");
+                result = false;
+            }
+            return result;
+        }
+        
+        /// <summary>
+        /// See if we have exceeded the configured depth.
+        /// If it isn't configured, put a stop at 20 just in case.
+        /// </summary>
+        private bool AssertTemplateStack(IInternalContextAdapter context)
+        {
+            bool result = true;
+            Object[] templateStack = context.TemplateNameStack;
+
+            if(templateStack.Length >= rsvc.GetInt(RuntimeConstants.PARSE_DIRECTIVE_MAXDEPTH, 20))
+            {
+                StringBuilder path = new StringBuilder();
+
+                for(int i = 0; i < templateStack.Length; ++i)
+                    path.Append(" > " + templateStack[i]);
+
+                rsvc.Error("Max recursion depth reached (" + templateStack.Length + ")" + " File stack:" + path);
+                result = false;
+            }
+            return result;
+        }
+
+
+        private Template GetTemplate(String arg, String encoding, IInternalContextAdapter context)
+        {
+            Template result = null;
+            try
+            {
+                result = rsvc.GetTemplate(arg, encoding);
+            }
+            catch(ResourceNotFoundException rnfe)
+            {
+                // the arg wasn't found.  Note it and throw
+                rsvc.Error("#parse(): cannot find template '" + arg + "', called from template " + context.CurrentTemplateName + " at (" + Line + ", " + Column + ")");
+                throw;
+            }
+            catch(ParseErrorException pee)
+            {
+                // the arg was found, but didn't parse - syntax error
+                // note it and throw
+                rsvc.Error("#parse(): syntax error in #parse()-ed template '" + arg + "', called from template " + context.CurrentTemplateName + " at (" + Line + ", " + Column + ")");
+
+                throw;
+            }
+            catch(Exception e)
+            {
+                rsvc.Error("#parse() : arg = " + arg + ".  Exception : " + e);
+                result = null;
+            }
+            return result;
+        }
+
+        private bool RenderTemplate(Template template, String arg, TextWriter writer, IInternalContextAdapter context)
+        {
+            bool result = true;
+            try
+            {
+                context.PushCurrentTemplateName(arg);
+                ((SimpleNode)template.Data).Render(context, writer);
+            }
+            catch(Exception e)
+            {
+                // if it's a MIE, it came from the render.... throw it...
+                if(e is MethodInvocationException)
+                    throw;
+
+                rsvc.Error("Exception rendering #parse( " + arg + " )  : " + e);
+                result = false;
+            }
+            finally
+            {
+                context.PopCurrentTemplateName();
+            }
+
+            return result;
+        }
 	}
 }
