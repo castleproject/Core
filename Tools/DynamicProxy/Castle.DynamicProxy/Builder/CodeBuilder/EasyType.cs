8fc27dfd2613f67d6d657f98d74b27ad0b53efe2
Ayende Rahien <ayende@ayende.com>
2006-06-05 03:32:20 +0000

    Added some debug.asserts to ensure correct behavior
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@1944 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Tools/DynamicProxy/Castle.DynamicProxy/Builder/CodeBuilder/EasyType.cs
+++ Tools/DynamicProxy/Castle.DynamicProxy/Builder/CodeBuilder/EasyType.cs
@@ -12,82 +12,92 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.Diagnostics;
+
 namespace Castle.DynamicProxy.Builder.CodeBuilder
 {
-	using System;
-	using System.Collections;
-	using System.Reflection;
+    using System;
+    using System.Collections;
+    using System.Reflection;
 
-	using Castle.DynamicProxy.Builder.CodeGenerators;
-	using Castle.DynamicProxy.Builder.CodeBuilder.SimpleAST;
-	using Castle.DynamicProxy.Builder.CodeBuilder.Utils;
+    using Castle.DynamicProxy.Builder.CodeGenerators;
+    using Castle.DynamicProxy.Builder.CodeBuilder.SimpleAST;
+    using Castle.DynamicProxy.Builder.CodeBuilder.Utils;
 
-	/// <summary>
-	/// Summary description for EasyType.
-	/// </summary>
-	[CLSCompliant(false)]
-	public class EasyType : AbstractEasyType
-	{
+    /// <summary>
+    /// Summary description for EasyType.
+    /// </summary>
+    [CLSCompliant(false)]
+    public class EasyType : AbstractEasyType
+    {
         static IDictionary signedAssemblyCache = new Hashtable();
 
-		protected EasyType() : base()
-		{
-		}
+        protected EasyType()
+            : base()
+        {
+        }
 
-		public EasyType( ModuleScope modulescope, String name, Type baseType, Type[] interfaces ) : 
-			this(modulescope, name, baseType, interfaces, false)
-		{
-		}
+        public EasyType(ModuleScope modulescope, String name, Type baseType, Type[] interfaces)
+            :
+            this(modulescope, name, baseType, interfaces, false)
+        {
+        }
 
-		public EasyType( ModuleScope modulescope, String name, Type baseType, Type[] interfaces, bool serializable ) : this()
-		{
-			TypeAttributes flags = 
-				TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.Serializable;
+        public EasyType(ModuleScope modulescope, String name, Type baseType, Type[] interfaces, bool serializable)
+            : this()
+        {
+            TypeAttributes flags =
+                TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.Serializable;
 
-			if (serializable)
-			{
-				flags |= TypeAttributes.Serializable;
-			}
+            if (serializable)
+            {
+                flags |= TypeAttributes.Serializable;
+            }
 
             bool isAssemblySigned = IsAssemblySigned(baseType);
-            
-			_typebuilder = modulescope.ObtainDynamicModule(isAssemblySigned).DefineType( 
-				name, flags, baseType, interfaces );
-		}
 
-		public EasyType( ModuleScope modulescope, String name ) : this(modulescope, name, typeof(object), new Type[0])
-		{
-		}
+            _typebuilder = modulescope.ObtainDynamicModule(isAssemblySigned).DefineType(
+                name, flags, baseType, interfaces);
+        }
 
-		public EasyCallable CreateCallable( ReturnReferenceExpression returnType, 
-			params ArgumentReference[] args )
-		{
-			EasyCallable nested = new EasyCallable( this, IncrementAndGetCounterValue, 
-				returnType, args );
-			_nested.Add( nested );
-			return nested;
-		}
+        public EasyType(ModuleScope modulescope, String name)
+            : this(modulescope, name, typeof(object), new Type[0])
+        {
+        }
 
-		public EasyCallable CreateCallable( Type returnType, params ParameterInfo[] args )
-		{
-			EasyCallable nested = new EasyCallable( this, IncrementAndGetCounterValue, 
-				new ReturnReferenceExpression(returnType), ArgumentsUtil.ConvertToArgumentReference(args) );
-			_nested.Add( nested );
-			return nested;
-		}
+        public EasyCallable CreateCallable(ReturnReferenceExpression returnType,
+            params ArgumentReference[] args)
+        {
+            EasyCallable nested = new EasyCallable(this, IncrementAndGetCounterValue,
+                returnType, args);
+            _nested.Add(nested);
+            return nested;
+        }
 
-		private bool IsAssemblySigned(Type baseType)
-		{
-			lock(signedAssemblyCache)
-			{
-				if (signedAssemblyCache.Contains(baseType.Assembly)==false)
-				{
-					byte[] key = baseType.Assembly.GetName().GetPublicKey();
-					bool isSigned = key !=null  && key.Length != 0;
-					signedAssemblyCache.Add(baseType.Assembly, isSigned );
-				}
-				return (bool) signedAssemblyCache[baseType.Assembly];
-			}
-		}
-	}
+        public EasyCallable CreateCallable(Type returnType, params ParameterInfo[] args)
+        {
+            EasyCallable nested = new EasyCallable(this, IncrementAndGetCounterValue,
+                new ReturnReferenceExpression(returnType), ArgumentsUtil.ConvertToArgumentReference(args));
+            _nested.Add(nested);
+            return nested;
+        }
+
+        private bool IsAssemblySigned(Type baseType)
+        {
+            Debug.Assert(baseType != null, "baseType should never be null");
+            lock (signedAssemblyCache)
+            {
+                if (signedAssemblyCache.Contains(baseType.Assembly) == false)
+                {
+                    AssemblyName name = baseType.Assembly.GetName();
+                    Debug.Assert(name !=null, "AssemblyName should never be null;");
+                    bool isSigned = false;
+                    byte[] key = name.GetPublicKey();
+                    isSigned = key != null && key.Length != 0;
+                    signedAssemblyCache.Add(baseType.Assembly, isSigned);
+                }
+                return (bool)signedAssemblyCache[baseType.Assembly];
+            }
+        }
+    }
 }
