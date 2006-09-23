2e0a5c79a5c553cfd201cd0fd30aafda26a4e080
Hamilton Verissimo <hammett@gmail.com>
2006-09-23 06:19:55 +0000

    Added test case
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@2375 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Components/General/Binder/Castle.Components.Binder.Tests/TreeBuilderTestCase.cs
+++ Components/General/Binder/Castle.Components.Binder.Tests/TreeBuilderTestCase.cs
@@ -188,5 +188,17 @@ namespace Castle.Components.Binder.Tests
 			Assert.AreEqual("hammett@apache.org", ((LeafNode)entries[1]).Value);
 			Assert.AreEqual("hammett@uol.com.br", ((LeafNode)entries[2]).Value);
 		}
+
+		[Test, ExpectedException(typeof(BindingException), "Attempt to create or obtain a composite node named Process, but a node with the same exists with the type Leaf")]
+		public void RepeatedNamesForNodes()
+		{
+			NameValueCollection nameValueColl = new NameValueCollection();
+			
+			nameValueColl.Add("profile.Process", "test");
+			nameValueColl.Add("profile.Process.Id", "1");
+
+			TreeBuilder builder = new TreeBuilder();
+			CompositeNode root = builder.BuildSourceNode(nameValueColl);
+		}
 	}
 }
