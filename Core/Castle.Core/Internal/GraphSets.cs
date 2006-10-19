7cb4ad8df002a2a4a7744a25b7b2fef7aa86a412
Ayende Rahien <ayende@ayende.com>
2006-10-19 20:52:39 +0000

    Adding XML comments to VertexColor
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@2786 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Core/Castle.Core/Internal/GraphSets.cs
+++ Core/Castle.Core/Internal/GraphSets.cs
@@ -19,8 +19,17 @@ namespace Castle.Core.Internal
 	internal enum VertexColor
 	{
 		NotInThisSet,
+		/// <summary>
+		/// The node has not been visited yet
+		/// </summary>
 		White,
+		/// <summary>
+		/// This node is in the process of being visited
+		/// </summary>
 		Gray,
+		/// <summary>
+		/// This now was visited
+		/// </summary>
 		Black
 	}
 
