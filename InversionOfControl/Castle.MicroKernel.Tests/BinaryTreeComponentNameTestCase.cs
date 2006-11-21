473a1ebe1ec8d52acac46c1001c57658447d470c
Hamilton Verissimo <hammett@gmail.com>
2006-11-21 12:30:22 +0000

    Reverted change that broken the build
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@2979 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- InversionOfControl/Castle.MicroKernel.Tests/BinaryTreeComponentNameTestCase.cs
+++ InversionOfControl/Castle.MicroKernel.Tests/BinaryTreeComponentNameTestCase.cs
@@ -15,8 +15,11 @@
 namespace Castle.MicroKernel.Tests
 {
 	using System;
+
 	using NUnit.Framework;
+
 	using Castle.Core;
+
 	using Castle.MicroKernel.Handlers;
 	using Castle.MicroKernel.SubSystems.Naming;
 
@@ -27,162 +30,61 @@ namespace Castle.MicroKernel.Tests
 		public void Usage()
 		{
 			BinaryTreeComponentName tree = new BinaryTreeComponentName();
+			
+			DefaultHandler handler1 = new DefaultHandler( 
+				new ComponentModel("A", typeof(DefaultHandler), typeof(DefaultHandler) ) );
+			DefaultHandler handler2 = new DefaultHandler( 
+				new ComponentModel("B", typeof(DefaultHandler), typeof(DefaultHandler) ) );
+			DefaultHandler handler3 = new DefaultHandler( 
+				new ComponentModel("C", typeof(DefaultHandler), typeof(DefaultHandler) ) );
+			DefaultHandler handler4 = new DefaultHandler( 
+				new ComponentModel("D", typeof(DefaultHandler), typeof(DefaultHandler) ) );
+			DefaultHandler handler5 = new DefaultHandler( 
+				new ComponentModel("E", typeof(DefaultHandler), typeof(DefaultHandler) ) );
+			DefaultHandler handler6 = new DefaultHandler( 
+				new ComponentModel("F", typeof(DefaultHandler), typeof(DefaultHandler) ) );
 
-			DefaultHandler handler1 = new DefaultHandler(
-				new ComponentModel("A", typeof(DefaultHandler), typeof(DefaultHandler)));
-			DefaultHandler handler2 = new DefaultHandler(
-				new ComponentModel("B", typeof(DefaultHandler), typeof(DefaultHandler)));
-			DefaultHandler handler3 = new DefaultHandler(
-				new ComponentModel("C", typeof(DefaultHandler), typeof(DefaultHandler)));
-			DefaultHandler handler4 = new DefaultHandler(
-				new ComponentModel("D", typeof(DefaultHandler), typeof(DefaultHandler)));
-			DefaultHandler handler5 = new DefaultHandler(
-				new ComponentModel("E", typeof(DefaultHandler), typeof(DefaultHandler)));
-			DefaultHandler handler6 = new DefaultHandler(
-				new ComponentModel("F", typeof(DefaultHandler), typeof(DefaultHandler)));
+			tree.Add( new ComponentName("protocolhandler"), handler1 );
+			tree.Add( new ComponentName("protocolhandler:key=1"), handler2 );
+			tree.Add( new ComponentName("protocolhandler:key=2"), handler3 );
+			tree.Add( new ComponentName("protocolhandler:key=2,secure=true"), handler4 );
+			tree.Add( new ComponentName("modelmanager"), handler5 );
+			tree.Add( new ComponentName("viewmanager"), handler6 );
 
-			tree.Add(new ComponentName("protocolhandler"), handler1);
-			tree.Add(new ComponentName("protocolhandler:key=1"), handler2);
-			tree.Add(new ComponentName("protocolhandler:key=2"), handler3);
-			tree.Add(new ComponentName("protocolhandler:key=2,secure=true"), handler4);
-			tree.Add(new ComponentName("modelmanager"), handler5);
-			tree.Add(new ComponentName("viewmanager"), handler6);
+			Assert.AreSame( handler1, tree.GetHandler( new ComponentName("protocolhandler") ) );
+			Assert.AreSame( handler2, tree.GetHandler( new ComponentName("protocolhandler:key=1") ) );
+			Assert.AreSame( handler3, tree.GetHandler( new ComponentName("protocolhandler:key=2") ) );
+			Assert.AreSame( handler4, tree.GetHandler( new ComponentName("protocolhandler:key=2,secure=true") ) );
+			Assert.AreSame( handler5, tree.GetHandler( new ComponentName("modelmanager") ) );
+			Assert.AreSame( handler6, tree.GetHandler( new ComponentName("viewmanager") ) );
 
-			Assert.AreSame(handler1, tree.GetHandler(new ComponentName("protocolhandler")));
-			Assert.AreSame(handler2, tree.GetHandler(new ComponentName("protocolhandler:key=1")));
-			Assert.AreSame(handler3, tree.GetHandler(new ComponentName("protocolhandler:key=2")));
-			Assert.AreSame(handler4, tree.GetHandler(new ComponentName("protocolhandler:key=2,secure=true")));
-			Assert.AreSame(handler5, tree.GetHandler(new ComponentName("modelmanager")));
-			Assert.AreSame(handler6, tree.GetHandler(new ComponentName("viewmanager")));
+			IHandler[] handlers = tree.GetHandlers( new ComponentName("protocolhandler") );
 
-			IHandler[] handlers = tree.GetHandlers(new ComponentName("protocolhandler"));
+			Assert.AreEqual( 4, handlers.Length );
+			Assert.AreSame( handler1, handlers[0] );
+			Assert.AreSame( handler2, handlers[1] );
+			Assert.AreSame( handler3, handlers[2] );
+			Assert.AreSame( handler4, handlers[3] );
 
-			Assert.AreEqual(4, handlers.Length);
-			Assert.AreSame(handler1, handlers[0]);
-			Assert.AreSame(handler2, handlers[1]);
-			Assert.AreSame(handler3, handlers[2]);
-			Assert.AreSame(handler4, handlers[3]);
+			handlers = tree.GetHandlers( new ComponentName("protocolhandler:*") );
 
-			handlers = tree.GetHandlers(new ComponentName("protocolhandler:*"));
+			Assert.AreEqual( 4, handlers.Length );
+			Assert.AreSame( handler1, handlers[0] );
+			Assert.AreSame( handler2, handlers[1] );
+			Assert.AreSame( handler3, handlers[2] );
+			Assert.AreSame( handler4, handlers[3] );
 
-			Assert.AreEqual(4, handlers.Length);
-			Assert.AreSame(handler1, handlers[0]);
-			Assert.AreSame(handler2, handlers[1]);
-			Assert.AreSame(handler3, handlers[2]);
-			Assert.AreSame(handler4, handlers[3]);
+			handlers = tree.GetHandlers( new ComponentName("protocolhandler:secure=true") );
 
-			handlers = tree.GetHandlers(new ComponentName("protocolhandler:secure=true"));
+			Assert.AreEqual( 1, handlers.Length );
+			Assert.AreSame( handler4, handlers[0] );
 
-			Assert.AreEqual(1, handlers.Length);
-			Assert.AreSame(handler4, handlers[0]);
+			handlers = tree.GetHandlers( new ComponentName("protocolhandler:key=2") );
 
-			handlers = tree.GetHandlers(new ComponentName("protocolhandler:key=2"));
-
-			Assert.AreEqual(2, handlers.Length);
-			Assert.AreSame(handler3, handlers[0]);
-			Assert.AreSame(handler4, handlers[1]);
+			Assert.AreEqual( 2, handlers.Length );
+			Assert.AreSame( handler3, handlers[0] );
+			Assert.AreSame( handler4, handlers[1] );
 		}
-
-
-		[Test]
-		public void ComponentNameEmptyProperties()
-		{
-			BinaryTreeComponentName tree = new BinaryTreeComponentName();
-			DefaultHandler handler1 = new DefaultHandler(new ComponentModel("A", typeof(DefaultHandler), typeof(DefaultHandler)));
-			DefaultHandler handler2 = new DefaultHandler(new ComponentModel("B", typeof(DefaultHandler), typeof(DefaultHandler)));
-			tree.Add(new ComponentName("protocolhandler:key=1"), handler2);
-			tree.Add(new ComponentName("protocolhandler"), handler1);
-
-			Assert.AreEqual(handler1, tree.GetHandler(new ComponentName("protocolhandler")));
-		}
-
-		[Test]
-		public void RemoveUnbalancedRoot()
-		{
-			BinaryTreeComponentName tree = new BinaryTreeComponentName();
-			DefaultHandler handler1 = new DefaultHandler(new ComponentModel("A", typeof(DefaultHandler), typeof(DefaultHandler)));
-			tree.Add(new ComponentName("1000"), handler1);
-			tree.Add(new ComponentName("7500"), handler1);
-			tree.Add(new ComponentName("6000"), handler1);
-			tree.Add(new ComponentName("2000"), handler1);
-
-			tree.Remove(new ComponentName("1000"));
-			assertRemoved(tree, 3, new ComponentName("1000"), new ComponentName("6000"));
-		}
-
-
-		[Test]
-		public void RemoveBalancedRoot()
-		{
-			BinaryTreeComponentName tree = new BinaryTreeComponentName();
-			DefaultHandler handler1 = new DefaultHandler(new ComponentModel("A", typeof(DefaultHandler), typeof(DefaultHandler)));
-			tree.Add(new ComponentName("1000"), handler1);
-			tree.Add(new ComponentName("0500"), handler1);
-			tree.Add(new ComponentName("6000"), handler1);
-			tree.Add(new ComponentName("2000"), handler1);
-
-			tree.Remove(new ComponentName("1000"));
-			assertRemoved(tree, 3, new ComponentName("1000"), new ComponentName("6000"));
-		}
-
-		[Test]
-		public void RemoveSibling()
-		{
-			BinaryTreeComponentName tree = new BinaryTreeComponentName();
-			DefaultHandler handler1 = new DefaultHandler(new ComponentModel("A", typeof(DefaultHandler), typeof(DefaultHandler)));
-
-			tree.Add(new ComponentName("1000"), handler1);
-			tree.Add(new ComponentName("0500"), handler1);
-			tree.Add(new ComponentName("0500:P=1"), handler1);
-			tree.Add(new ComponentName("0500:p=2"), handler1);
-
-			tree.Remove(new ComponentName("0500:p=2"));
-			assertRemoved(tree, 3, new ComponentName("0500:P=2"), new ComponentName("0500:P=1"));
-		}
-
-
-		[Test]
-		public void RemoveLeaf()
-		{
-			BinaryTreeComponentName tree = new BinaryTreeComponentName();
-			DefaultHandler handler1 = new DefaultHandler(new ComponentModel("A", typeof(DefaultHandler), typeof(DefaultHandler)));
-
-			tree.Add(new ComponentName("1000"), handler1);
-			tree.Add(new ComponentName("0500"), handler1);
-
-			tree.Remove(new ComponentName("0500"));
-			assertRemoved(tree, 1, new ComponentName("0500"), new ComponentName("1000"));
-		}
-
-		[Test]
-		public void RemoveNodeWithSiblings()
-		{
-			BinaryTreeComponentName tree = new BinaryTreeComponentName();
-			DefaultHandler handler1 = new DefaultHandler(new ComponentModel("A", typeof(DefaultHandler), typeof(DefaultHandler)));
-
-			tree.Add(new ComponentName("1000"), handler1);
-			tree.Add(new ComponentName("0500"), handler1);
-			tree.Add(new ComponentName("0500:P=1"), handler1);
-			tree.Add(new ComponentName("0500:p=2"), handler1);
-			tree.Add(new ComponentName("0400"), handler1);
-
-			tree.Remove(new ComponentName("0500"));
-			Assert.AreEqual(4, tree.Count);
-			Assert.AreEqual(4, tree.Handlers.Length);
-			Assert.AreEqual(2, tree.GetHandlers(new ComponentName("0500")).Length);
-			Assert.IsNotNull(tree.GetHandler(new ComponentName("0500:p=2")));
-		}
-
-
-		private void assertRemoved(BinaryTreeComponentName tree, int expectedCount, ComponentName removed,
-		                           ComponentName exists)
-		{
-			Assert.AreEqual(expectedCount, tree.Count);
-			Assert.AreEqual(expectedCount, tree.Handlers.Length);
-			Assert.IsNull(tree.GetHandler(removed));
-			Assert.IsNotNull(tree.GetHandler(exists));
-		}
-
 
 		[Test]
 		public void AccessEmptyTree()
@@ -190,5 +92,6 @@ namespace Castle.MicroKernel.Tests
 			BinaryTreeComponentName tree = new BinaryTreeComponentName();
 			Assert.IsFalse(tree.Contains(new ComponentName("Test")));
 		}
+
 	}
 }
--- InversionOfControl/Castle.MicroKernel/SubSystems/Naming/BinaryTreeComponentName.cs
+++ InversionOfControl/Castle.MicroKernel/SubSystems/Naming/BinaryTreeComponentName.cs
@@ -49,7 +49,7 @@ namespace Castle.MicroKernel.SubSystems.Naming
 
 		public void Remove(ComponentName name)
 		{
-			Remove(FindNode(name));
+			// Not implemented yet
 		}
 
 		public void Add(ComponentName name, IHandler handler)
@@ -190,105 +190,8 @@ namespace Castle.MicroKernel.SubSystems.Naming
 				}
 			}
 		}
-
-		private void Remove(TreeNode node)
-		{
-			if (node == null) return;
-
-			//A real tree node (this should be better indicated, there is really two types of nodes here)
-			if (node.PreviousSibling == null)
-			{
-				if (node.NextSibling != null)
-				{
-					TreeNode replacement = node.NextSibling;
-					replacement.Right = node.Right;
-					replacement.Left = node.Left;
-					replacement.PreviousSibling = null;
-					ReplaceNode(node, replacement);
-				}
-				else
-				{
-					RemoveBinaryTreeNode(node);
-				}
-			}
-			else
-			{
-				node.PreviousSibling.NextSibling = node.NextSibling;
-			}
-
-			count--;
-		}
-
-		private TreeNode FindSuccessor(TreeNode node)
-		{
-			TreeNode current = node.Right;
-			
-			if (current != null)
-			{
-				while (current.Left != null)
-				{
-					current = current.Left;
-				}
-			}
-
-			return current;
-		}
-
-		private void PromoteNode(TreeNode oldNode, TreeNode promoted)
-		{
-			if (promoted.Right != null || promoted.Left != null)
-			{
-				throw new InvalidOperationException("promoted node can not have child nodes, remove node from tree before promoting");
-			}
-
-			promoted.Right = oldNode.Right;
-			promoted.Left = oldNode.Left;
-
-			if (root == oldNode) root = promoted;
-		}
-
-		private void ReplaceNode(TreeNode oldNode, TreeNode newNode)
-		{
-			TreeNode parent = oldNode.Parent;
-			
-			if(parent == null)
-			{
-				root = newNode;
-			}
-			else if (parent.Right == oldNode)
-			{
-				parent.Right = newNode;
-			}
-			else if (parent.Left == oldNode)
-			{
-				parent.Left = newNode;
-			}
-
-			//throw if wanted
-		}
-
-		private void RemoveBinaryTreeNode(TreeNode node)
-		{
-			if(node.Left != null && node.Right != null)
-			{
-				TreeNode inorderSuccessor = FindSuccessor(node);
-				RemoveBinaryTreeNode(inorderSuccessor); //it has maximum one node to reorder
-				PromoteNode(node, inorderSuccessor);
-			}
-			else if(node.Left == null && node.Right == null)
-			{
-				ReplaceNode(node, null);
-			}
-			else if(node.Left != null)
-			{
-				ReplaceNode(node, node.Left);
-			}
-			else if(node.Right != null)
-			{
-				ReplaceNode(node, node.Right);
-			}
-		}
 	}
+
 
 	[Serializable]
 	internal class TreeNode
@@ -301,13 +204,9 @@ namespace Castle.MicroKernel.SubSystems.Naming
 
 		/// <summary>Node's right</summary>
 		private TreeNode right;
-
-		/// <summary>Node's parent</summary>
-		private TreeNode parent;
 		
 		/// <summary>DA Linked List</summary>
 		private TreeNode nextSibling;
-		private TreeNode previousSibling;
 
 		public TreeNode(ComponentName compName, IHandler handler)
 		{
@@ -328,52 +227,22 @@ namespace Castle.MicroKernel.SubSystems.Naming
 			get { return handler; }
 		}
 
-		public TreeNode Parent
-		{
-			get { return parent; }
-			set { parent = value; }
-		}
-
 		public TreeNode Left
 		{
 			get { return left; }
-			set 
-			{ 
-				left = value;
-				if (left != null)
-				{
-					left.Parent = this;
-				}
-			}
+			set { left = value; }
 		}
 
 		public TreeNode Right
 		{
 			get { return right; }
-			set 
-			{ 
-				right = value;
-				if (right != null)
-				{
-					right.Parent = this;
-				}
-			}
+			set { right = value; }
 		}
 
 		public TreeNode NextSibling
 		{
 			get { return nextSibling; }
-			set
-			{
-				nextSibling = value;
-				if (nextSibling != null) nextSibling.PreviousSibling = this;
-			}
-		}
-
-		public TreeNode PreviousSibling
-		{
-			get { return previousSibling; }
-			set { previousSibling = value; }
+			set { nextSibling = value; }
 		}
 
 		public void AddSibling(TreeNode node)
@@ -395,46 +264,20 @@ namespace Castle.MicroKernel.SubSystems.Naming
 
 		public TreeNode FindBestMatch(ComponentName name)
 		{
-			if ("*".Equals(name.LiteralProperties))
-			{
-				return this;
-			}
-			else if(string.IsNullOrEmpty(name.LiteralProperties))
-			{
-				return FindWithEmptyProperties();
-			}
-			else
-			{
-				return FindBestMatchByProperties(name);
-			}
-		}
-
-		private TreeNode FindWithEmptyProperties()
-		{
 			TreeNode current = this;
 
-			while (current != null)
+			while(current != null)
 			{
-				if (string.IsNullOrEmpty(current.CompName.LiteralProperties))
-					return current;
+				if ("*".Equals(name.LiteralProperties))
+				{
+					break;
+				}
 
-				current = current.NextSibling;
-			}
-
-			return this;
-		}
-
-		private TreeNode FindBestMatchByProperties(ComponentName name)
-		{
-			TreeNode current = this;
-
-			while (current != null)
-			{
 				bool selected = true;
 
-				foreach (DictionaryEntry entry in name.Properties)
+				foreach(DictionaryEntry entry in name.Properties)
 				{
-					String value = current.CompName.Properties[entry.Key] as String;
+					String value = current.CompName.Properties[ entry.Key ] as String;
 
 					if (value == null || !value.Equals(entry.Value))
 					{
