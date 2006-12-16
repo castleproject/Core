6575799c5060e8973d4fb617dfd55abbb3af58ad
Fábio Batista <fabio.david.batista@gmail.com>
2006-12-16 19:52:34 +0000

    Added failing test case for NVelocity, regarding NVELOCITY-8.
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@3110 73e77b4c-caa6-f847-a29a-24ab75ae54b6

---
+++ Tools/NVelocity/src/NVelocity.Tests/Test/MethodCallTestCase.cs
@@ -0,0 +1,95 @@
+// Copyright 2004-2006 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace NVelocity.Test
+{
+	using System;
+	using System.Globalization;
+	using System.IO;
+
+	using NUnit.Framework;
+
+	using NVelocity.App;
+
+	[TestFixture]
+	public class MethodCallTestCase
+	{
+		private VelocityEngine ve;
+		private VelocityContext c;
+
+		[TestFixtureSetUp]
+		public void StartNVelocity()
+		{
+			ve = new VelocityEngine();
+			ve.Init();
+		}
+
+		[SetUp]
+		public void ResetContext()
+		{
+			c = new VelocityContext();
+			c.Put("test", new TestClass());
+		}
+
+		[Test]
+		public void HasExactSignature()
+		{
+			double num = 55.0;
+			c.Put("num", num);
+			Assert.AreEqual("55", Eval("$test.justDoIt($num)"));
+		}
+
+		[Test]
+		public void HasCompatibleSignature()
+		{
+			int num = 99;
+			c.Put("num", num);
+			Assert.AreEqual("99", Eval("$test.justDoIt($num)"));
+		}
+
+		[Test]
+		public void HasRelaxedSignature()
+		{
+			string path = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+			DirectoryInfo di = new DirectoryInfo(path);
+			c.Put("di", di);
+			Assert.AreEqual(path, Eval("$test.justDoIt($di)"));
+		}
+
+		public class TestClass
+		{
+			public string JustDoIt(double obj)
+			{
+				return Convert.ToString(obj, CultureInfo.InvariantCulture);
+			}
+
+			public string JustDoIt(object obj)
+			{
+				return Convert.ToString(obj, CultureInfo.InvariantCulture);
+			}
+		}
+
+		private string Eval(string template)
+		{
+			using(StringWriter sw = new StringWriter())
+			{
+				bool ok = ve.Evaluate(c, sw, "ContextTest.CaseInsensitive", template);
+
+				Assert.IsTrue(ok, "Evalutation returned failure");
+
+				return sw.ToString();
+			}
+		}
+	}
+}
