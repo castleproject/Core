db2f3ea59412892215fa9dd1fba8d0eec59402b0
Hamilton Verissimo <hammett@gmail.com>
2007-02-22 02:42:58 +0000

    - Added eval support to configuration. Currently it only supports   BaseDirectory as a content to evaluate
      <?eval $BaseDirectory ?>
    
    
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@3549 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- InversionOfControl/Castle.Windsor/Configuration/Interpreters/XmlProcessor/XmlProcessor.cs
+++ InversionOfControl/Castle.Windsor/Configuration/Interpreters/XmlProcessor/XmlProcessor.cs
@@ -69,6 +69,7 @@ namespace Castle.Windsor.Configuration.Interpreters.XmlProcessor
 			AddElementProcessor(typeof(DefinedProcessingInstructionProcessor));
 			AddElementProcessor(typeof(UndefProcessingInstructionProcessor));
 			AddElementProcessor(typeof(DefaultTextNodeProcessor));
+			AddElementProcessor(typeof(EvalProcessingInstructionProcessor));
 		}
 
 		protected void AddElementProcessor(Type t)
