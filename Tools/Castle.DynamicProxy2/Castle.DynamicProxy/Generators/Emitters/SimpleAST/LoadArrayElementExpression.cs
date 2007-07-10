cc7aaef10d7823acbb9a41f7f0b48e82d229b7e1
Fabian Schmied <fabian.schmied@gmail.com>
2007-07-10 11:08:18 +0000

    Fixed bug in LoadArrayElementExpression, see DYNPROXY-57
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@4045 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Tools/Castle.DynamicProxy2/Castle.DynamicProxy/Generators/Emitters/SimpleAST/LoadArrayElementExpression.cs
+++ Tools/Castle.DynamicProxy2/Castle.DynamicProxy/Generators/Emitters/SimpleAST/LoadArrayElementExpression.cs
@@ -22,23 +22,25 @@ namespace Castle.DynamicProxy.Generators.Emitters.SimpleAST
 	{
 		private readonly ConstReference index;
 		private readonly Reference arrayReference;
+		private readonly Type returnType;
 
-		public LoadArrayElementExpression(int index, Reference arrayReference)
-			: this(new ConstReference(index), arrayReference)
+		public LoadArrayElementExpression (int index, Reference arrayReference, Type returnType)
+			: this (new ConstReference (index), arrayReference, returnType)
 		{
 		}
 
-		public LoadArrayElementExpression(ConstReference index, Reference arrayReference)
+		public LoadArrayElementExpression (ConstReference index, Reference arrayReference, Type returnType)
 		{
 			this.index = index;
 			this.arrayReference = arrayReference;
+			this.returnType = returnType;
 		}
 
-		public override void Emit(IMemberEmitter member, ILGenerator gen)
+		public override void Emit (IMemberEmitter member, ILGenerator gen)
 		{
-			ArgumentsUtil.EmitLoadOwnerAndReference(arrayReference, gen);
-			ArgumentsUtil.EmitLoadOwnerAndReference(index, gen);
-			gen.Emit(OpCodes.Ldelem);
+			ArgumentsUtil.EmitLoadOwnerAndReference (arrayReference, gen);
+			ArgumentsUtil.EmitLoadOwnerAndReference (index, gen);
+			gen.Emit (OpCodes.Ldelem, returnType);
 		}
 	}
 }
