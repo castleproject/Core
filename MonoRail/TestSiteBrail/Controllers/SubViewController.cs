4bf3dbebc3e058f0abe561a371cd7ec59cc88b49
Ayende Rahien <ayende@ayende.com>
2007-11-22 21:55:29 +0000

    Fixing broken test. Enabling Brail tests in the default build.
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@4513 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- MonoRail/TestSiteBrail/Controllers/SubViewController.cs
+++ MonoRail/TestSiteBrail/Controllers/SubViewController.cs
@@ -14,54 +14,55 @@
 
 namespace Castle.MonoRail.Views.Brail.TestSite.Controllers
 {
-    using Castle.MonoRail.Framework;
-    using Castle.MonoRail.Views.Brail;
-    using System;
-    using System.Collections;
-    using System.IO;
-    using System.Reflection;
+	using Castle.MonoRail.Framework;
+	using Castle.MonoRail.Views.Brail;
+	using System;
+	using System.Collections;
+	using System.IO;
+	using System.Reflection;
 
-    [Serializable]
-    public class SubViewController : Controller
-    {
-        public void Index()
-        {
-        }
+	[Serializable]
+	public class SubViewController : Controller
+	{
+		public void Index()
+		{
+		}
 
-        public void SubViewWithLayout()
-        {
-            this.LayoutName = "master";
-            this.RenderView("index");
-        }
+		public void SubViewWithLayout()
+		{
+			this.LayoutName = "master";
+			this.RenderView("index");
+		}
 
-        public void SubViewWithParameters()
-        {
-            this.RenderView("CallSubViewWithParameters");
-        }
+		public void SubViewWithParameters()
+		{
+			this.RenderView("CallSubViewWithParameters");
+		}
 
-        public void SubViewWithPath()
-        {
-        }
+		public void SubViewWithPath()
+		{
+		}
 
-        public void useLotsOfSubViews()
-        {
-            //this is ugly, but the other way is to open up things that really shouldn't be opened...
-            IViewEngineManager viewEngineManager = (IViewEngineManager)this.ServiceProvider.GetService(typeof(IViewEngineManager));
-            IViewEngine engine1 = (IViewEngine)viewEngineManager.GetType().GetMethod("ResolveEngine", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(viewEngineManager, new object[] { "dummy.brail" });
-            Hashtable hashtable1 = (Hashtable) typeof(BooViewEngine).GetField("compilations", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(engine1);
-            Hashtable hashtable2 = (Hashtable) typeof(BooViewEngine).GetField("constructors", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(engine1);
-            if (this.Context.Request.QueryString["replaceSubView"] == "reset")
-            {
-                hashtable1[@"subview\listItem.brail"] = null;
-            }
-            else if (this.Context.Request.QueryString["replaceSubView"] == "true")
-            {
-                hashtable1[@"subview\listItem.brail"] = typeof(DummySubView);
-                Type[] typeArray1 = new Type[] { typeof(BooViewEngine), typeof(TextWriter), typeof(IRailsEngineContext), typeof(Controller) };
-                hashtable2[typeof(DummySubView)] = typeof(DummySubView).GetConstructor(typeArray1);
-            }
-        }
+		public void useLotsOfSubViews()
+		{
+			//this is ugly, but the other way is to open up things that really shouldn't be opened...
+			IViewEngineManager viewEngineManager = (IViewEngineManager)this.ServiceProvider.GetService(typeof(IViewEngineManager));
+			MethodInfo method = viewEngineManager.GetType().GetMethod("ResolveEngine", BindingFlags.Instance|BindingFlags.NonPublic, null,new Type[] { typeof(string) },new ParameterModifier[0]);
+			IViewEngine engine1 = (IViewEngine)method.Invoke(viewEngineManager, new object[] { "dummy.brail" });
+			Hashtable hashtable1 = (Hashtable)typeof(BooViewEngine).GetField("compilations", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(engine1);
+			Hashtable hashtable2 = (Hashtable)typeof(BooViewEngine).GetField("constructors", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(engine1);
+			if (this.Context.Request.QueryString["replaceSubView"] == "reset")
+			{
+				hashtable1[@"subview\listItem.brail"] = null;
+			}
+			else if (this.Context.Request.QueryString["replaceSubView"] == "true")
+			{
+				hashtable1[@"subview\listItem.brail"] = typeof(DummySubView);
+				Type[] typeArray1 = new Type[] { typeof(BooViewEngine), typeof(TextWriter), typeof(IRailsEngineContext), typeof(Controller) };
+				hashtable2[typeof(DummySubView)] = typeof(DummySubView).GetConstructor(typeArray1);
+			}
+		}
 
-    }
+	}
 }
 
