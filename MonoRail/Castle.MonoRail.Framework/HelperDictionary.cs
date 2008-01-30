654dabc5c9a5eacae01dce312d11110472ea42a0
Hamilton Verissimo <hammett@gmail.com>
2008-01-30 03:26:12 +0000

    A helper doesn't necessarily extend from AbstractHelper
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@4771 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- MonoRail/Castle.MonoRail.Framework/HelperDictionary.cs
+++ MonoRail/Castle.MonoRail.Framework/HelperDictionary.cs
@@ -1,8 +1,21 @@
-using System.Collections.Specialized;
-using Castle.MonoRail.Framework.Helpers;
+// Copyright 2004-2008 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 namespace Castle.MonoRail.Framework
 {
+	using System.Collections.Specialized;
+
 	/// <summary>
 	/// Dictionary containing helpers.
 	/// </summary>
@@ -19,7 +32,7 @@ namespace Castle.MonoRail.Framework
 		/// Adds the supplied helper using the standard key naming rules.
 		/// </summary>
 		/// <param name="helper">The helper to be added</param>
-		public void Add(AbstractHelper helper)
+		public void Add(object helper)
 		{
 			string helperName = helper.GetType().Name;
 
