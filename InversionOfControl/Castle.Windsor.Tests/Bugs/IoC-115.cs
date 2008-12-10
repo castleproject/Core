b805519c5e01868e4e7e118e3a4728ce033bec36
Ayende Rahien <ayende@ayende.com>
2008-12-10 05:09:22 +0000

    Adding passing test for IoC-115
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@5437 73e77b4c-caa6-f847-a29a-24ab75ae54b6

---
+++ InversionOfControl/Castle.Windsor.Tests/Bugs/IoC-115.cs
@@ -0,0 +1,86 @@
+using Castle.Windsor;
+using NUnit.Framework;
+
+namespace Castle.Windsor.Tests.Bugs
+{
+    [TestFixture]
+    public class IoC_115
+    {
+        [Test]
+        public void  Cannot_resolve_a_dependency_from_a_parent_container_under_certain_circumstances()
+        {
+            WindsorContainer parent = new WindsorContainer();
+            WindsorContainer child = new WindsorContainer();
+
+            parent.AddChildContainer(child);
+
+            parent.AddComponent("service", typeof(IParentService), typeof(ParentService));
+            child.AddComponent("service1", typeof(IChildService1), typeof(ChildService1));
+            child.AddComponent("service2", typeof(IChildService2), typeof(ChildService2));
+
+
+            child.Resolve<IChildService1>();
+        }
+
+
+        [Test]
+        public void Should_resolve_child_from_childs_container()
+        {
+            WindsorContainer parent = new WindsorContainer();
+            WindsorContainer child = new WindsorContainer();
+
+            parent.AddChildContainer(child);
+
+            parent.AddComponent("service1", typeof(IParentService), typeof(ParentService));
+            child.AddComponent("service2", typeof(IParentService), typeof(AnotherParentService));
+            child.AddComponent("service3", typeof(IChildService2), typeof(ChildService2));
+
+
+            var resolve = child.Resolve<IChildService2>();
+            Assert.IsInstanceOfType(typeof(AnotherParentService),resolve.Parent);
+        }
+
+        public interface IParentService
+        {
+        }
+
+        public class ParentService : IParentService
+        {
+        }
+
+        public interface IChildService1
+        {
+        }
+
+        public class ChildService1 : IChildService1
+        {
+            public ChildService1(IChildService2 xxx)
+            {
+            }
+        }
+
+        public interface IChildService2
+        {
+            IParentService Parent { get; }
+        }
+
+        public class ChildService2 : IChildService2
+        {
+            private readonly IParentService xxx;
+
+            public ChildService2(IParentService xxx)
+            {
+                this.xxx = xxx;
+            }
+
+            public IParentService Parent
+            {
+                get { return xxx; }
+            }
+        }
+
+        public class AnotherParentService : IParentService
+        {
+        }
+    }
+}
