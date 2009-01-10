08da325acf225489de307db8c821eb11423aad0b
Craig Neuwirt <cneuwirt@gmail.com>
2009-01-10 17:55:15 +0000

    Added test cases to demonstrate issue with proxying generic methods using additional interfaces.
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@5501 73e77b4c-caa6-f847-a29a-24ab75ae54b6

---
+++ Tools/Castle.DynamicProxy2/Castle.DynamicProxy.Tests/GenericMethodsProxyTestCase.cs
@@ -0,0 +1,38 @@
+// Copyright 2004-2008 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+namespace Castle.DynamicProxy.Tests
+{
+	using System;
+	using Castle.Core.Interceptor;
+	using Castle.DynamicProxy.Tests.GenInterfaces;
+	using Castle.DynamicProxy.Tests.InterClasses;
+	using NUnit.Framework;
+
+
+	[TestFixture]
+	public class GenericMethodsProxyTestCase : BasePEVerifyTestCase
+	{
+		[Test, Ignore("This is broken")]
+		public void ProxyAdditionalInterfaceWithGenericMethods()
+		{
+			IService proxy = (IService)generator.CreateInterfaceProxyWithoutTarget(
+				typeof(IService), new Type[] { typeof(OnlyGenMethodsInterface) }, 
+				new StandardInterceptor());
+
+			Assert.IsNotNull(proxy);
+		}
+	}
+}
