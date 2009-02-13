2fa81fbbc214a3bc8df2fed3f8f855dc9c2b62f0
Ken Egozi <mail@kenegozi.com>
2009-02-13 07:16:40 +0000

    [COMP-ISSUE-82] - refactoring DefaultConverter - applying patch from Mike Nichols
    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@5546 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Components/Binder/Castle.Components.Binder/DefaultConverter.cs
+++ Components/Binder/Castle.Components.Binder/DefaultConverter.cs
@@ -15,64 +15,84 @@
 namespace Castle.Components.Binder
 {
 	using System;
-	using System.Collections;
 	using System.Collections.Generic;
-	using System.ComponentModel;
-	using System.Web;
 
 	public class DefaultConverter : MarshalByRefObject, IConverter
 	{
-		public bool CanConvert(Type desiredType, Type inputType, object input, out bool exactMatch)
+		private readonly List<IConverter> converters = new List<IConverter>();
+
+		public DefaultConverter()
+		{
+			Initialize();
+		}
+		private void Initialize()
+		{
+			if(converters.Count==0)
+			{
+				AddConverter(new StringConverter());
+				AddConverter(new ExactMatchConverter());
+				AddConverter(new ArrayConverter(this));
+				AddConverter(new EnumConverter());
+				AddConverter(new DecimalConverter());
+				AddConverter(new PrimitiveConverter());
+				AddConverter(new NullableConverter(this));
+				AddConverter(new GuidConverter());
+				AddConverter(new DateTimeConverter());
+				AddConverter(new HttpPostedFileConverter());
+				AddConverter(new GenericListConverter(this));
+				AddConverter(new TypeConverterAdapter());
+
+			}
+		}
+
+		public virtual void AddConverter(IConverter converter)
+		{
+			converters.Add(converter);
+		}
+		private IConverter FindConverter(Type desiredType,object input)
+		{
+			bool exactMatch;
+			var converter = converters.Find(c => c.CanConvert(desiredType, null, input, out exactMatch));
+			if(converter==null)
+			{
+				throw new BindingException("Unable to find converter for '{0}'", desiredType);
+			}
+			return converter;
+			
+		}
+		public virtual bool CanConvert(Type desiredType, Type inputType, object input, out bool exactMatch)
 		{
 			exactMatch = false;
-
-			if (input == null)
+			foreach (var converter in converters)
 			{
-				return true;
+				if(converter.CanConvert(desiredType,inputType,input,out exactMatch))
+				{
+					return true;
+				}
 			}
-			else if (inputType == desiredType)
-			{
-				exactMatch = true;
-			}
-			else if (!desiredType.IsInstanceOfType(input))
-			{
-				TypeConverter conv = TypeDescriptor.GetConverter(desiredType);
-				return (conv != null && conv.CanConvertFrom(inputType));
-			}
-
 			return false;
 		}
 
-		public object Convert(Type desiredType, Type inputType, object input, out bool conversionSucceeded)
+		public virtual object Convert(Type desiredType, Type inputType, object input, out bool conversionSucceeded)
 		{
-			if (inputType == desiredType) // Nothing to convert
+			try
 			{
-				if (inputType == typeof(String))
-				{
-					if (input == null)
-					{
-						conversionSucceeded = false;
-						return null;
-					}
-					else if (input.ToString().Length == 0)
-					{
-						conversionSucceeded = true;
-						return null;
-					}
-					else
-					{
-						conversionSucceeded = true;
-						return input.ToString().Trim(' ');
-					}
-				}
-				else
-				{
-					conversionSucceeded = true;
-					return input;
-				}
+				conversionSucceeded = (input != null);
+				var converterImpl = FindConverter(desiredType, input);
+				return converterImpl.Convert(desiredType, null, input, out conversionSucceeded);
+			}
+			catch (BindingException)
+			{
+				throw;
+			}
+			catch (Exception inner)
+			{
+				conversionSucceeded = false;
+
+				ThrowInformativeException(desiredType, input, inner);
+				return null;
 			}
 
-			return Convert(desiredType, input, out conversionSucceeded);
 		}
 
 		/// <summary>
@@ -92,440 +112,8 @@ namespace Castle.Components.Binder
 		/// </remarks>
 		public object Convert(Type desiredType, object input, out bool conversionSucceeded)
 		{
-			try
-			{
-				conversionSucceeded = (input != null);
-
-				if (desiredType.IsInstanceOfType(input))
-				{
-					return input;
-				}
-				else if (desiredType == typeof(String))
-				{
-					if (conversionSucceeded && input.GetType() == typeof(string) && ((String) input) == String.Empty)
-					{
-						return null;
-					}
-
-					return conversionSucceeded ? input.ToString().Trim(' ') : null;
-				}
-				else if (desiredType.IsArray)
-				{
-					return conversionSucceeded
-					       	?
-					       		ConvertToArray(desiredType, input, ref conversionSucceeded)
-					       	: null;
-				}
-				else if (desiredType.IsEnum)
-				{
-					return ConvertEnum(desiredType, input, ref conversionSucceeded);
-				}
-				else if (desiredType == typeof(Decimal))
-				{
-					return ConvertDecimal(input, ref conversionSucceeded);
-				}
-				else if (desiredType.IsPrimitive)
-				{
-					return ConvertPrimitive(desiredType, input, ref conversionSucceeded);
-				}
-				else if (desiredType.IsGenericType &&
-				         desiredType.GetGenericTypeDefinition() == typeof(Nullable<>))
-				{
-					return ConvertNullable(desiredType, input, ref conversionSucceeded);
-				}
-				else if (desiredType == typeof(Guid))
-				{
-					return ConvertGuid(input, ref conversionSucceeded);
-				}
-				else if (desiredType == typeof(DateTime))
-				{
-					return ConvertDate(input, ref conversionSucceeded);
-				}
-				else if (desiredType == typeof(HttpPostedFile))
-				{
-					return input;
-				}
-				else if (DataBinder.IsGenericList(desiredType))
-				{
-					return conversionSucceeded
-					       	? ConvertGenericList(desiredType, input, ref conversionSucceeded)
-					       	: null;
-				}
-				else
-				{
-					return ConvertUsingTypeConverter(desiredType, input, ref conversionSucceeded);
-				}
-			}
-			catch(BindingException)
-			{
-				throw;
-			}
-			catch(Exception inner)
-			{
-				conversionSucceeded = false;
-
-				ThrowInformativeException(desiredType, input, inner);
-				return null;
-			}
+			return Convert(desiredType, null, input, out conversionSucceeded);
 		}
-
-		private object ConvertNullable(Type desiredType, object input, ref bool conversionSucceeded)
-		{
-			Type underlyingType = Nullable.GetUnderlyingType(desiredType);
-
-			object value = Convert(underlyingType, input, out conversionSucceeded);
-
-			if (conversionSucceeded && value != null)
-			{
-				Type typeToConstruct = typeof(Nullable<>).MakeGenericType(underlyingType);
-
-				return Activator.CreateInstance(typeToConstruct, value);
-			}
-			else if (input != null)
-			{
-				conversionSucceeded = true;
-			}
-			else
-			{
-				conversionSucceeded = false;
-			}
-
-			return null;
-		}
-
-		private object ConvertGenericList(Type desiredType, object input, ref bool conversionSucceeded)
-		{
-			Type elemType = desiredType.GetGenericArguments()[0];
-
-			input = FixInputForMonoIfNeeded(elemType, input);
-
-			Type listType = typeof(List<>).MakeGenericType(elemType);
-			IList result = (IList) Activator.CreateInstance(listType);
-			Array values = input as Array;
-
-			bool elementConversionSucceeded;
-
-			for(int i = 0; i < values.Length; i++)
-			{
-				object val = Convert(elemType, values.GetValue(i), out elementConversionSucceeded);
-				if(val!=null)
-				{
-					result.Add(val);
-				}
-
-				// if at least one list element get converted 
-				// we consider the conversion a success
-				if (elementConversionSucceeded && !conversionSucceeded)
-				{
-					conversionSucceeded = true;
-				}
-			}
-
-			return result;
-		}
-
-		/// <summary>
-		/// Fix for mod_mono issue where array values are passed as a comma seperated String.
-		/// </summary>
-		/// <param name="elemType"></param>
-		/// <param name="input"></param>
-		/// <returns></returns>
-		private static object FixInputForMonoIfNeeded(Type elemType, object input)
-		{
-			if (!input.GetType().IsArray)
-			{
-				if (input.GetType() == typeof(String))
-				{
-					input = NormalizeInput(input);
-
-					if (input.ToString() == String.Empty)
-					{
-						input = Array.CreateInstance(elemType, 0);
-					}
-					else
-					{
-						input = input.ToString().Split(',');
-					}
-				}
-				else
-				{
-					throw new BindingException("Cannot convert to collection of {0} from type {1}", elemType.FullName,
-					                           input.GetType().FullName);
-				}
-			}
-
-			return input;
-		}
-
-		private object ConvertGuid(object input, ref bool conversionSucceeded)
-		{
-			conversionSucceeded = true;
-
-			if (input == null)
-			{
-				conversionSucceeded = false;
-				return null;
-			}
-
-			String value = NormalizeInput(input);
-
-			if (value == String.Empty)
-			{
-				conversionSucceeded = true;
-				return null;
-			}
-			else
-			{
-				return new Guid(value);
-			}
-		}
-
-		private object ConvertDecimal(object input, ref bool conversionSucceeded)
-		{
-			conversionSucceeded = true;
-
-			if (input == null)
-			{
-				conversionSucceeded = false;
-				return null;
-			}
-
-			String value = NormalizeInput(input);
-
-			if (value == String.Empty)
-			{
-				conversionSucceeded = true;
-				return null;
-			}
-			else
-			{
-				return System.Convert.ToDecimal(value);
-			}
-		}
-
-		private object ConvertPrimitive(Type desiredType, object input, ref bool conversionSucceeded)
-		{
-			conversionSucceeded = true;
-
-			String value = NormalizeInput(input);
-
-			if (IsBool(desiredType))
-			{
-				return SpecialBoolConversion(value, input, ref conversionSucceeded);
-			}
-			else if (input == null)
-			{
-				conversionSucceeded = false;
-				return null;
-			}
-			else if (value == String.Empty)
-			{
-				conversionSucceeded = true;
-				return null;
-			}
-			else
-			{
-				return System.Convert.ChangeType(input, desiredType);
-			}
-		}
-
-		private static object SpecialBoolConversion(string value, object input, ref bool conversionSucceeded)
-		{
-			if (input == null)
-			{
-				conversionSucceeded = false;
-				return null;
-			}
-			else if (value == String.Empty)
-			{
-				conversionSucceeded = false;
-				return false;
-			}
-			else
-			{
-				if (value.IndexOf(',') != -1)
-				{
-					value = value.Substring(0, value.IndexOf(','));
-				}
-
-				bool performNumericConversion = false;
-
-				foreach(char c in value.ToCharArray())
-				{
-					if (Char.IsNumber(c))
-					{
-						performNumericConversion = true;
-						break;
-					}
-				}
-
-				if (performNumericConversion)
-				{
-					return System.Convert.ToBoolean(System.Convert.ToInt32(value));
-				}
-				else
-				{
-					return !(String.Compare("false", value, true) == 0);
-				}
-			}
-		}
-
-		private object ConvertToArray(Type desiredType, object input, ref bool conversionSucceeded)
-		{
-			Type elemType = desiredType.GetElementType();
-
-			input = FixInputForMonoIfNeeded(elemType, input);
-
-			Array values = input as Array;
-			Array result = Array.CreateInstance(elemType, values.Length);
-
-			for(int i = 0; i < values.Length; i++)
-			{
-				bool elementConversionSucceeded;
-
-				result.SetValue(Convert(elemType, values.GetValue(i), out elementConversionSucceeded), i);
-
-				// if at least one array element get converted 
-				// we consider the conversion a success
-				if (elementConversionSucceeded && !conversionSucceeded)
-				{
-					conversionSucceeded = true;
-				}
-			}
-
-			return result;
-		}
-
-		private static bool IsBool(Type desiredType)
-		{
-			bool isBool = desiredType == typeof(Boolean);
-
-			if (!isBool)
-			{
-				isBool = desiredType == typeof(bool?);
-			}
-
-			return isBool;
-		}
-
-		private static object ConvertEnum(Type desiredType, object input, ref bool conversionSucceeded)
-		{
-			conversionSucceeded = true;
-
-			if (input == null)
-			{
-				conversionSucceeded = false;
-				return null;
-			}
-
-			String value = NormalizeInput(input);
-
-			if (value == String.Empty)
-			{
-				conversionSucceeded = false;
-				return null;
-			}
-			else
-			{
-				return Enum.Parse(desiredType, value, true);
-			}
-		}
-
-		private static object ConvertDate(object input, ref bool conversionSucceeded)
-		{
-			conversionSucceeded = true;
-
-			if (input == null)
-			{
-				conversionSucceeded = false;
-				return null;
-			}
-
-			String value = NormalizeInput(input);
-
-			if (value == String.Empty)
-			{
-				conversionSucceeded = false;
-
-				return null;
-			}
-			else
-			{
-				return DateTime.Parse(value);
-			}
-		}
-
-		/// <summary>
-		/// Support for types that specify a TypeConverter, 
-		/// i.e.: NullableTypes
-		/// </summary>
-		private static object ConvertUsingTypeConverter(Type desiredType, object input, ref bool conversionSucceeded)
-		{
-			conversionSucceeded = true;
-
-			Type sourceType = (input != null ? input.GetType() : typeof(String));
-			TypeConverter conv = TypeDescriptor.GetConverter(desiredType);
-
-			if (conv != null && conv.CanConvertFrom(sourceType))
-			{
-				try
-				{
-					return conv.ConvertFrom(input);
-				}
-				catch(Exception ex)
-				{
-					String message = String.Format("Conversion error: " +
-					                               "Could not convert parameter with value '{0}' to {1}", input, desiredType);
-
-					throw new BindingException(message, ex);
-				}
-			}
-			else
-			{
-				String message = String.Format("Conversion error: " +
-				                               "Could not convert parameter with value '{0}' to {1}", input, desiredType);
-
-				throw new BindingException(message);
-			}
-		}
-
-		private static String NormalizeInput(object input)
-		{
-			if (input == null)
-			{
-				return String.Empty;
-			}
-			else
-			{
-				if (!input.GetType().IsArray)
-				{
-					return input.ToString().Trim();
-				}
-				else
-				{
-					Array array = (Array) input;
-
-					String[] stArray = new string[array.GetLength(0)];
-
-					for(int i = 0; i < stArray.Length; i++)
-					{
-						object itemVal = array.GetValue(i);
-
-						if (itemVal != null)
-						{
-							stArray[i] = itemVal.ToString();
-						}
-						else
-						{
-							stArray[i] = String.Empty;
-						}
-					}
-
-					return String.Join(",", stArray);
-				}
-			}
-		}
-
 		private static void ThrowInformativeException(Type desiredType, object input, Exception inner)
 		{
 			String message = String.Format("Conversion error: " +
@@ -534,5 +122,6 @@ namespace Castle.Components.Binder
 
 			throw new BindingException(message, inner);
 		}
+
 	}
 }
