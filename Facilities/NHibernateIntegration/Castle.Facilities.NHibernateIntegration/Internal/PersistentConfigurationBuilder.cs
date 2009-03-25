f6cf8d41ba3af95a9842662e1f5b7bc99bccdbe7
Tuna Toksoz <tehlike@gmail.com>
2009-03-25 06:30:48 +0000

    git-svn-id: http://svn.castleproject.org:8080/svn/castle/trunk@5601 73e77b4c-caa6-f847-a29a-24ab75ae54b6

--- Facilities/NHibernateIntegration/Castle.Facilities.NHibernateIntegration/Internal/PersistentConfigurationBuilder.cs
+++ Facilities/NHibernateIntegration/Castle.Facilities.NHibernateIntegration/Internal/PersistentConfigurationBuilder.cs
@@ -1,31 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-using Castle.Core.Configuration;
-using NHibernate.Cfg;
-using System.Runtime.Serialization.Formatters.Binary;
-using log4net;
-using NHibernate.Type;
+﻿// Copyright 2004-2008 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Text.RegularExpressions;
 
 namespace Castle.Facilities.NHibernateIntegration.Internal
 {
+	using System.Collections.Generic;
+	using Castle.Core.Configuration;
+	using NHibernate.Cfg;
+	using log4net;
+
 	/// <summary>
 	/// Serializes the Configuration for subsequent initializations.
 	/// </summary>
-	public class PersistentConfigurationBuilder:DefaultConfigurationBuilder
+	public class PersistentConfigurationBuilder : DefaultConfigurationBuilder
 	{
+		private const string DEFAULT_EXTENSION = "dat";
+
 		private static readonly ILog log = LogManager.GetLogger(typeof(PersistentConfigurationBuilder));
-		private BinaryFormatter binaryFormatter;
+
+		private readonly IConfigurationPersister configurationPersister;
+
 		/// <summary>
-		/// Initializes the presistent <see cref="Configuration"/> builder.
+		/// Initializes the presistent <see cref="Configuration"/> builder
+		/// with an specific <see cref="IConfigurationPersister"/>
+		/// </summary>
+		public PersistentConfigurationBuilder(IConfigurationPersister configurationPersister)
+		{
+			this.configurationPersister = configurationPersister;
+		}
+
+		/// <summary>
+		/// Initializes the presistent <see cref="Configuration"/> builder
+		/// using the default <see cref="IConfigurationPersister"/>
 		/// </summary>
 		public PersistentConfigurationBuilder()
+			: this(new DefaultConfigurationPersister())
 		{
-			this.binaryFormatter = new BinaryFormatter();
 		}
-		#region IConfigurationBuilder Members
+
 		/// <summary>
 		/// Returns the Deserialized Configuration
 		/// </summary>
@@ -35,71 +59,57 @@ namespace Castle.Facilities.NHibernateIntegration.Internal
 		{
 			log.Debug("Building the Configuration");
 
-			string fileName = config.Attributes["fileName"];
-
-			IConfiguration dependsOn = config.Children["dependsOn"];
-			IList<string> list = new List<string>();
-
-			foreach (var on in dependsOn.Children)
-				list.Add(on.Value);
+			string filename = GetFilenameFrom(config);
+			IList<string> dependentFilenames = GetDependentFilenamesFrom(config);
 
 			Configuration cfg;
-			if (IsNewConfigurationRequired(fileName, list))
+			if (configurationPersister.IsNewConfigurationRequired(filename, dependentFilenames))
 			{
 				log.Debug("Configuration is either old or some of the dependencies have changed");
-				using(var fileStream = new FileStream(fileName, FileMode.OpenOrCreate))
-				{
-					cfg = base.GetConfiguration(config);
-					this.WriteConfigurationToStream(fileStream, cfg);
-				}
+				cfg = base.GetConfiguration(config);
+				configurationPersister.WriteConfiguration(filename, cfg);
 			}
 			else
 			{
-				using (var fileStream = new FileStream(fileName, FileMode.OpenOrCreate))
-				{
-					cfg = this.GetConfigurationFromStream(fileStream);
-				}
+				cfg = configurationPersister.ReadConfiguration(filename);
 			}
 			return cfg;
 		}
-		/// <summary>
-		/// Writes the <see cref="Configuration"/> to stream
-		/// </summary>
-		/// <param name="stream">The stream to be written</param>
-		/// <param name="cfg"></param>
-		protected virtual void WriteConfigurationToStream(Stream stream,Configuration cfg)
+
+		private string GetFilenameFrom(IConfiguration config)
 		{
-			binaryFormatter.Serialize(stream, cfg);
-		}
-		/// <summary>
-		/// Gets the <see cref="Configuration"/> from stream.
-		/// </summary>
-		/// <param name="fs">The stream from which the configuration will be deserialized</param>
-		/// <returns>The <see cref="Configuration"/></returns>
-		protected virtual Configuration GetConfigurationFromStream(Stream fs)
-		{
-			return binaryFormatter.Deserialize(fs) as Configuration;
+			var filename = config.Attributes["fileName"] ?? config.Attributes["id"] + "." + DEFAULT_EXTENSION;
+			return StripInvalidCharacters(filename);
 		}
 
-		/// <summary>
-		/// Checks if a new <see cref="Configuration"/> is required or a serialized one should be used.
-		/// </summary>
-		/// <param name="fileName">The file that contains serialized <see cref="Configuration"/></param>
-		/// <param name="dependencies">Files that the serialized configuration depends on. </param>
-		/// <returns>If the <see cref="Configuration"/> should be created or not.</returns>
-		protected virtual bool IsNewConfigurationRequired(string fileName,IList<string> dependencies)
+		private string StripInvalidCharacters(string input)
 		{
-			if (!File.Exists(fileName))
-				return true;
-			DateTime lastModified = File.GetLastWriteTime(fileName);
-			bool requiresNew=false;
-			for (int i = 0; i < dependencies.Count && !requiresNew; i++)
-			{
-				DateTime dependencyLastModified = File.GetLastWriteTime(dependencies[i]);
-				requiresNew |= dependencyLastModified > lastModified;
-			}
-			return requiresNew;
+			return Regex.Replace(input, "[:*?\"<>\\\\/]", "", RegexOptions.IgnoreCase);
 		}
-		#endregion
+
+		private IList<string> GetDependentFilenamesFrom(IConfiguration config)
+		{
+			IList<string> list = new List<string>();
+
+			IConfiguration assemblies = config.Children["assemblies"];
+			if (assemblies != null)
+			{
+				foreach (var assembly in assemblies.Children)
+				{
+					list.Add(assembly.Value + ".dll");
+				}
+			}
+			
+			IConfiguration dependsOn = config.Children["dependsOn"];
+			if (dependsOn != null)
+			{
+				foreach (var on in dependsOn.Children)
+				{
+					list.Add(on.Value);
+				}
+			}
+
+			return list;
+		}
 	}
 }
