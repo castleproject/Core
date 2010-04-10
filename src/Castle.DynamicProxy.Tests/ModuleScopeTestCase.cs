d2c2cb2c4084ef1c550bb133c8a18139c219ecf4
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-04-10 16:51:02 +0200

    - moved content of Dynamic Proxy project into Core project
    - removed Dynamic Proxy project - Dynamic Proxy now lives in Core

--- src/Castle.DynamicProxy.Tests/ModuleScopeTestCase.cs
+++ src/Castle.DynamicProxy.Tests/ModuleScopeTestCase.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-using System.Collections;
 using System.Collections.Generic;
 using Castle.DynamicProxy.Generators;
 using Castle.DynamicProxy.Tests.InterClasses;
