d4389d94acd2e40702c2a60f681795a3d0779513
Craig Neuwirt <cneuwirt@gmail.com>
2010-08-10 14:12:52 -0500

    Added GenericDictionaryAdapter to adapter generic dictionary with string key

---
+++ src/Castle.Core/Components.DictionaryAdapter/GenericDictionaryAdapter.cs
@@ -0,0 +1,40 @@
+namespace Castle.Components.DictionaryAdapter
+{
+	using System;
+	using System.Collections.Generic;
+
+	public class GenericDictionaryAdapter<TValue> : AbstractDictionaryAdapter
+	{
+		private readonly IDictionary<string, TValue> _dictionary;
+
+		public GenericDictionaryAdapter(IDictionary<string, TValue> dictionary)
+		{
+			_dictionary = dictionary;
+		}
+
+		public override bool IsReadOnly
+		{
+			get { return _dictionary.IsReadOnly; }
+		}
+
+		public override bool Contains(object key)
+		{
+			return _dictionary.Keys.Contains(GetKey(key));
+		}
+
+		public override object this[object key]
+		{
+			get { return _dictionary[GetKey(key)]; }
+			set { _dictionary[GetKey(key)] = (TValue)value; }
+		}
+
+		private static string GetKey(object key)
+		{
+			if (key == null)
+			{
+				throw new ArgumentNullException("key");
+			}
+			return key.ToString();
+		}
+	}
+}
