446c7b54dce01bcc106bf1633aa5ee2a4230e2d4
Craig Neuwirt <cneuwirt@gmail.com>
2010-08-13 12:59:37 -0500

    Added support for dictionary of string values

--- Castle.Core.Tests/Components.DictionaryAdapter.Tests/AdaptingGenericDictionaryTestCase.cs
+++ Castle.Core.Tests/Components.DictionaryAdapter.Tests/AdaptingGenericDictionaryTestCase.cs
@@ -9,7 +9,7 @@ namespace Castle.Components.DictionaryAdapter.Tests
 		[Test]
 		public void Should_Handle_String_Object()
 		{
-			IDictionary<string, object> dictionary = new Dictionary<string, object>();
+			var dictionary = new Dictionary<string, object>();
 			dictionary.Add("Team", "Giants");
 			dictionary.Add("Points", 48);
 
@@ -21,12 +21,22 @@ namespace Castle.Components.DictionaryAdapter.Tests
 		[Test]
 		public void Should_Handle_String_String()
 		{
-			IDictionary<string, string> dictionary = new Dictionary<string, string>();
+			var dictionary = new Dictionary<string, string>();
 			dictionary.Add("Team", "Giants");
 			dictionary.Add("Points", "23");
 
 			var player = _factory.GetAdapter<IPlayer, string>(dictionary);
 			Assert.AreEqual("Giants", player.Team);
+			Assert.AreEqual(23, player.Points);
+		}
+
+		[Test]
+		public void Can_Save_Everything_As_A_String()
+		{
+			var dictionary = new Dictionary<string, string>();
+			var player = (IPlayer)_factory.GetAdapter(typeof(IPlayer), dictionary.ForDictionaryAdapter(),
+				new PropertyDescriptor().AddSetter(new StringStorageAttribute()));
+			player.Points = 23;
 			Assert.AreEqual(23, player.Points);
 		}
 
