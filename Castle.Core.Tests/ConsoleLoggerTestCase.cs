47480e347b8e91c34973a35726f587ddce76bff8
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-08-21 21:39:54 +1000

    - got rid of not used linked list
    - some clean up in test project

--- Castle.Core.Tests/ConsoleLoggerTestCase.cs
+++ Castle.Core.Tests/ConsoleLoggerTestCase.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if !SILVERLIGHT
+#if !SILVERLIGHT // In Silverlight Console.SetOut throws security exception
 namespace Castle.Core.Logging.Tests
 {
 	using System;
