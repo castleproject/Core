708ade7de870ac8f67bd5395796af9a690cf05a6
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-09-20 19:38:07 +1000

    - updated changes.txt

--- Castle.Core.Tests/SlimReadWriteLockTestCase.cs
+++ Castle.Core.Tests/SlimReadWriteLockTestCase.cs
@@ -1,11 +1,23 @@
+// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 namespace Castle.Core.Tests
 {
 	using System.Threading;
-
 	using Castle.Core.Internal;
-
 	using NUnit.Framework;
-	
+
 #if !SILVERLIGHT
 	[TestFixture]
 	public class SlimReadWriteLockTestCase
@@ -21,9 +33,9 @@ namespace Castle.Core.Tests
 		[Test]
 		public void Can_be_used_ForReading_multiple_nested_time()
 		{
-			using( @lock.ForReading())
+			using (@lock.ForReading())
 			{
-				using(@lock.ForReading())
+				using (@lock.ForReading())
 				{
 					Assert.IsTrue(@lock.IsReadLockHeld);
 				}
@@ -33,7 +45,6 @@ namespace Castle.Core.Tests
 		[Test]
 		public void Can_be_used_ForWriting_multiple_nested_time()
 		{
-
 			using (@lock.ForWriting())
 			{
 				using (@lock.ForWriting())
@@ -46,7 +57,6 @@ namespace Castle.Core.Tests
 		[Test]
 		public void Can_be_used_ForReadingUpgradeable_multiple_nested_time()
 		{
-
 			using (@lock.ForReadingUpgradeable())
 			{
 				using (@lock.ForReadingUpgradeable())
