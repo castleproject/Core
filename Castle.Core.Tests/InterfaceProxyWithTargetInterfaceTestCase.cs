1d175eccac99280d28801c0a0eb9c98fdf08c5dc
Krzysztof Kozmic <krzysztof@kozmic.pl>
2010-09-20 19:29:59 +1000

    - enabled null as valid value for interfaceproxywithtargetinterface

--- Castle.Core.Tests/InterfaceProxyWithTargetInterfaceTestCase.cs
+++ Castle.Core.Tests/InterfaceProxyWithTargetInterfaceTestCase.cs
@@ -15,10 +15,8 @@
 namespace Castle.DynamicProxy.Tests
 {
 	using System;
-
 	using Castle.DynamicProxy.Tests.Interceptors;
 	using Castle.DynamicProxy.Tests.Interfaces;
-
 	using NUnit.Framework;
 
 	[TestFixture]
@@ -27,21 +25,22 @@ namespace Castle.DynamicProxy.Tests
 		[Test]
 		public void When_target_does_not_implement_additional_interface_method_should_throw()
 		{
-			var proxy = generator.CreateInterfaceProxyWithTargetInterface(typeof (IOne),
-																		  new[] {typeof (ITwo)},
-																		  new One(),
-																		  ProxyGenerationOptions.Default,new StandardInterceptor());
-			Assert.Throws(typeof(NotImplementedException),() => (proxy as ITwo).TwoMethod());
+			var proxy = generator.CreateInterfaceProxyWithTargetInterface(typeof(IOne),
+			                                                              new[] {typeof(ITwo)},
+			                                                              new One(),
+			                                                              ProxyGenerationOptions.Default,
+			                                                              new StandardInterceptor());
+			Assert.Throws(typeof(NotImplementedException), () => (proxy as ITwo).TwoMethod());
 		}
 
 		[Test]
 		public void When_target_does_implement_additional_interface_method_should_forward()
 		{
 			var proxy = generator.CreateInterfaceProxyWithTargetInterface(typeof(IOne),
-																		  new[] { typeof(ITwo) },
-																		  new OneTwo(),
-																		  ProxyGenerationOptions.Default);
-			int result = (proxy as ITwo).TwoMethod();
+			                                                              new[] {typeof(ITwo)},
+			                                                              new OneTwo(),
+			                                                              ProxyGenerationOptions.Default);
+			var result = (proxy as ITwo).TwoMethod();
 			Assert.AreEqual(2, result);
 		}
 
@@ -49,10 +48,10 @@ namespace Castle.DynamicProxy.Tests
 		public void TargetInterface_methods_should_be_forwarded_to_target()
 		{
 			var proxy = generator.CreateInterfaceProxyWithTargetInterface(typeof(IOne),
-																		  new[] { typeof(ITwo) },
-																		  new OneTwo(),
-																		  ProxyGenerationOptions.Default);
-			int result = (proxy as IOne).OneMethod();
+			                                                              new[] {typeof(ITwo)},
+			                                                              new OneTwo(),
+			                                                              ProxyGenerationOptions.Default);
+			var result = (proxy as IOne).OneMethod();
 			Assert.AreEqual(3, result);
 		}
 
@@ -64,7 +63,7 @@ namespace Castle.DynamicProxy.Tests
 			var proxy = generator.CreateInterfaceProxyWithTargetInterface(typeof(IOne),
 			                                                              new OneTwo(),
 			                                                              options);
-			int result = (proxy as ITwo).TwoMethod();
+			var result = (proxy as ITwo).TwoMethod();
 			Assert.AreEqual(2, result);
 		}
 
@@ -85,19 +84,42 @@ namespace Castle.DynamicProxy.Tests
 		}
 
 		[Test]
+		public void Null_target_is_valid()
+		{
+			var interceptor = new ChangeTargetInterceptor(new OneTwo());
+			generator.CreateInterfaceProxyWithTargetInterface(typeof(IOne),
+			                                                  default(object),
+			                                                  new ProxyGenerationOptions(),
+			                                                  interceptor);
+		}
+
+		[Test]
+		public void Null_target_can_be_replaced()
+		{
+			var options = new ProxyGenerationOptions();
+			options.AddMixinInstance(new Two());
+			var interceptor = new ChangeTargetInterceptor(new OneTwo());
+			var proxy = generator.CreateInterfaceProxyWithTargetInterface(typeof(IOne),
+			                                                              default(object),
+			                                                              options,
+			                                                              interceptor);
+
+			Assert.AreEqual(3, ((IOne) proxy).OneMethod());
+		}
+
+		[Test]
 		public void Should_detect_and_report_setting_null_as_target_for_Mixin_methods()
 		{
 			var options = new ProxyGenerationOptions();
 			options.AddMixinInstance(new Two());
 			var interceptor = new ChangeTargetInterceptor(null);
 			var proxy = generator.CreateInterfaceProxyWithTargetInterface(typeof(IOne),
-																		  new One(),
-																		  options,
-																		  interceptor);
+			                                                              new One(),
+			                                                              options,
+			                                                              interceptor);
 
-			Assert.Throws(typeof(NotImplementedException),()=>
-
-			(proxy as ITwo).TwoMethod());
+			Assert.Throws(typeof(NotImplementedException), () =>
+			                                               (proxy as ITwo).TwoMethod());
 		}
 
 		[Test]
@@ -105,7 +127,7 @@ namespace Castle.DynamicProxy.Tests
 		{
 			var interceptor = new ChangeProxyTargetInterceptor(new OneTwo());
 			var proxy = generator.CreateInterfaceProxyWithTargetInterface<IOne>(new One(), interceptor);
-			
+
 			proxy.OneMethod();
 
 			var type = GetTargetType(proxy);
