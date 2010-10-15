c0f6cbf7e35381fbcc41698b2fdb5edd74791772
Roelof Blom <roelof.blom@gmail.com>
2010-10-15 17:23:18 +0200

    Exclude stuff from Silverlight build

--- Castle.Core/Components.DictionaryAdapter/IXPathSerializer.cs
+++ Castle.Core/Components.DictionaryAdapter/IXPathSerializer.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#if !SILVERLIGHT
+
 namespace Castle.Components.DictionaryAdapter
 {
 	using System.Xml.XPath;
@@ -23,3 +25,4 @@ namespace Castle.Components.DictionaryAdapter
 		bool ReadObject(XPathResult result, XPathNavigator node, out object value);
 	}
 }
+#endif
