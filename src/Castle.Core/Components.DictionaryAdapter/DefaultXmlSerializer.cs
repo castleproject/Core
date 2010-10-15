09b308180f2fe08cad8356df36896e82af2552a8
Roelof Blom <roelof.blom@gmail.com>
2010-10-15 17:23:18 +0200

    Exclude stuff from Silverlight build

--- src/Castle.Core/Components.DictionaryAdapter/DefaultXmlSerializer.cs
+++ src/Castle.Core/Components.DictionaryAdapter/DefaultXmlSerializer.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#if !SILVERLIGHT
+
 namespace Castle.Components.DictionaryAdapter
 {
 	using System.Text;
@@ -85,3 +87,4 @@ namespace Castle.Components.DictionaryAdapter
 		}
 	}
 }
+#endif
