1614857e059a97d17f16564266b0e6f74bcac49c
Roelof Blom <roelof.blom@gmail.com>
2010-10-26 14:25:53 +0200

    Modified the Log4netFactoryTestCase to use a MemoryAppender

--- Castle.Core.Tests/log4netIntegration/Log4netFactoryTestCase.cs
+++ Castle.Core.Tests/log4netIntegration/Log4netFactoryTestCase.cs
@@ -1,7 +1,24 @@
-using System;
+// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using System.IO;
+using System.Linq;
 using System.Text;
 using Castle.Services.Logging.Log4netIntegration;
+using log4net;
+using log4net.Appender;
+using log4net.Repository.Hierarchy;
 using NUnit.Framework;
 
 namespace Castle.log4netIntegration
@@ -10,28 +27,25 @@ namespace Castle.log4netIntegration
 	public class Log4netFactoryTestCase
 	{
 		private const string logMessage = "testing log4net configuration using a stream for configuration";
-		private const string logFileName = "unit-test-log-file.txt";
-		private const string loggerName = "UseMessageOnlyFileLog";
+		private const string loggerName = "Log4netFactoryTestCase";
 
 		[Test]
 		public void CanCreateStreamFromString()
 		{
 			string original = log4netConfig.Config;
-			using (var stream = StringToStream(original))
-			using (var reader = new StreamReader(stream))
+			using (Stream stream = StringToStream(original))
 			{
-				string roundTrip = reader.ReadToEnd();
+				using (var reader = new StreamReader(stream))
+				{
+					string roundTrip = reader.ReadToEnd();
 
-				Assert.AreEqual(original, roundTrip);
-			}
+					Assert.AreEqual(original, roundTrip);
+				}}
 		}
 
 		[Test]
 		public void CanCreateLog4NetConfigUsingStream()
 		{
-			// Test with writing to a file
-			// Just using console output doesn't work as the test runner writes to the console too
-
 			Log4netFactory factory;
 			using (var stream = StringToStream(log4netConfig.Config))
 			{
@@ -41,7 +55,7 @@ namespace Castle.log4netIntegration
 			var logger = factory.Create(loggerName);
 			logger.Debug(logMessage);
 
-			var logContent = File.ReadAllText(logFileName);
+			var logContent = GetLogContent();
 
 			Assert.AreEqual(logMessage, logContent);
 		}
@@ -58,18 +72,17 @@ namespace Castle.log4netIntegration
 			var logger = factory.Create(loggerName);
 			logger.Debug(logMessage);
 
-			var logContent = File.ReadAllText(logFileName);
+			var logContent = GetLogContent();
 
 			Assert.AreEqual(logMessage, logContent);
 		}
 
-		[TearDown]
-		public  void TearDown()
+		private string GetLogContent()
 		{
-			if (File.Exists(logFileName))
-			{
-				File.Delete(logFileName);
-			}
+			var repository = (Hierarchy)LogManager.GetRepository();
+			var memoryAppender = (from appender in repository.GetAppenders().OfType<MemoryAppender>() select appender).Single();
+
+			return memoryAppender.GetEvents()[0].RenderedMessage;
 		}
 
 		private static Stream StringToStream(string s)
