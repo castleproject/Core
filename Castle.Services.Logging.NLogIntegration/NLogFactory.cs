fb5a80f11c5e2f60d95b0c53be21e7d8c92db30a
Roelof Blom <roelof.blom@gmail.com>
2010-10-27 11:20:41 +0200

    Added NLog for .NET 4.0, .NET 3.5, Silverlight 4.0 and Silverlight 3.0 Enabled Castle.Services.Logging.NLogIntegration on Silverlight builds.

--- Castle.Services.Logging.NLogIntegration/NLogFactory.cs
+++ Castle.Services.Logging.NLogIntegration/NLogFactory.cs
@@ -21,25 +21,47 @@ namespace Castle.Services.Logging.NLogIntegration
 	using NLog;
 	using NLog.Config;
 
+	/// <summary>
+	/// Implementation of <see cref="ILoggerFactory"/> for NLog.
+	/// </summary>
 	public class NLogFactory : AbstractLoggerFactory
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NLogFactory"/> class.
+		/// </summary>
 		public NLogFactory()
 			: this("nlog.config")
 		{
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NLogFactory"/> class.
+		/// </summary>
+		/// <param name="configFile">The config file.</param>
 		public NLogFactory(string configFile)
 		{
 			FileInfo file = GetConfigFile(configFile);
 			LogManager.Configuration = new XmlLoggingConfiguration(file.FullName);
 		}
 
+		/// <summary>
+		/// Creates a logger with specified <paramref name="name"/>.
+		/// </summary>
+		/// <param name="name">The name.</param>
+		/// <returns></returns>
 		public override ILogger Create(String name)
 		{
 			Logger log = LogManager.GetLogger(name);
 			return new NLogLogger(log, this);
 		}
 
+		/// <summary>
+		/// Not implemented, NLog logger levels cannot be set at runtime.
+		/// </summary>
+		/// <param name="name">The name.</param>
+		/// <param name="level">The level.</param>
+		/// <returns></returns>
+		/// <exception cref="NotImplementedException" />
 		public override ILogger Create(String name, LoggerLevel level)
 		{
 			throw new NotImplementedException("Logger levels cannot be set at runtime. Please review your configuration file.");
