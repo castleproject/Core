9ba78561d32ecf6d0f01597535227bb1e1a87b58
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-05-07 16:18:56 +1000

    fixed (kind of)CORE-37 - TAB characters in the XML Configuration of a component parameter is read as String.Empty

--- src/Castle.Core.Tests/log4netIntegration/Log4netFactoryTestCase.cs
+++ src/Castle.Core.Tests/log4netIntegration/Log4netFactoryTestCase.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2010 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,37 +12,59 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if !SILVERLIGHT
 
-using System.IO;
-using System.Linq;
-using System.Text;
-using Castle.Services.Logging.Log4netIntegration;
-using log4net;
-using log4net.Appender;
-using log4net.Repository.Hierarchy;
-using NUnit.Framework;
+#if !SILVERLIGHT
 
 namespace Castle.log4netIntegration
 {
+	using System.IO;
+	using System.Linq;
+	using System.Text;
+
+	using Castle.Services.Logging.Log4netIntegration;
+
+	using CastleTests.log4netIntegration;
+
+	using NUnit.Framework;
+
+	using log4net;
+	using log4net.Appender;
+	using log4net.Repository.Hierarchy;
+
 	[TestFixture]
 	public class Log4netFactoryTestCase
 	{
 		private const string logMessage = "testing log4net configuration using a stream for configuration";
 		private const string loggerName = "Log4netFactoryTestCase";
 
-		[Test]
-		public void CanCreateStreamFromString()
+		private string GetLogContent()
 		{
-			string original = log4netConfig.Config;
-			using (Stream stream = StringToStream(original))
-			{
-				using (var reader = new StreamReader(stream))
-				{
-					string roundTrip = reader.ReadToEnd();
+			var repository = (Hierarchy)LogManager.GetRepository();
+			var memoryAppender = (from appender in repository.GetAppenders().OfType<MemoryAppender>() select appender).Single();
 
-					Assert.AreEqual(original, roundTrip);
-				}}
+			return memoryAppender.GetEvents()[0].RenderedMessage;
+		}
+
+		private static Stream StringToStream(string s)
+		{
+			return new MemoryStream(Encoding.Default.GetBytes(s));
+		}
+
+		[Test]
+		public void CanCreateExtendedLog4NetConfigUsingStream()
+		{
+			ExtendedLog4netFactory factory;
+			using (var stream = StringToStream(log4netConfig.Config))
+			{
+				factory = new ExtendedLog4netFactory(stream);
+			}
+
+			var logger = factory.Create(loggerName);
+			logger.Debug(logMessage);
+
+			var logContent = GetLogContent();
+
+			Assert.AreEqual(logMessage, logContent);
 		}
 
 		[Test]
@@ -63,34 +85,20 @@ namespace Castle.log4netIntegration
 		}
 
 		[Test]
-		public void CanCreateExtendedLog4NetConfigUsingStream()
+		public void CanCreateStreamFromString()
 		{
-			ExtendedLog4netFactory factory;
-			using (var stream = StringToStream(log4netConfig.Config))
+			var original = log4netConfig.Config;
+			using (var stream = StringToStream(original))
 			{
-				factory = new ExtendedLog4netFactory(stream);
+				using (var reader = new StreamReader(stream))
+				{
+					var roundTrip = reader.ReadToEnd();
+
+					Assert.AreEqual(original, roundTrip);
+				}
 			}
-
-			var logger = factory.Create(loggerName);
-			logger.Debug(logMessage);
-
-			var logContent = GetLogContent();
-
-			Assert.AreEqual(logMessage, logContent);
-		}
-
-		private string GetLogContent()
-		{
-			var repository = (Hierarchy)LogManager.GetRepository();
-			var memoryAppender = (from appender in repository.GetAppenders().OfType<MemoryAppender>() select appender).Single();
-
-			return memoryAppender.GetEvents()[0].RenderedMessage;
-		}
-
-		private static Stream StringToStream(string s)
-		{
-			return new MemoryStream(Encoding.Default.GetBytes(s));
 		}
 	}
 }
+
 #endif
