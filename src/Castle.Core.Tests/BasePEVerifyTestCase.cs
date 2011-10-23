c2a0dad75ea87f32b5b82bdf0fbe1319a7d9c197
Krzysztof Kozmic <krzysztof@kozmic.pl>
2011-10-23 20:35:53 +1000

    - fixing pe-verify issues

--- src/Castle.Core.Tests/BasePEVerifyTestCase.cs
+++ src/Castle.Core.Tests/BasePEVerifyTestCase.cs
@@ -12,6 +12,132 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+namespace CastleTests
+{
+	using System;
+	using System.Diagnostics;
+	using System.IO;
+
+	using Castle.DynamicProxy;
+
+	using CastleTests.Properties;
+
+	using NUnit.Framework;
+
+#if !MONO && !SILVERLIGHT
+	// mono doesn't have PEVerify
+	[SetUpFixture]
+	public class FindPeVerify
+	{
+		[SetUp]
+		public void FindPeVerifySetUp()
+		{
+			var peVerifyProbingPaths = Settings.Default.PeVerifyProbingPaths;
+			foreach (var path in peVerifyProbingPaths)
+			{
+				var file = Path.Combine(path, "peverify.exe");
+				if (File.Exists(file))
+				{
+					PeVerifyPath = file;
+					return;
+				}
+			}
+			throw new FileNotFoundException(
+				"Please check the PeVerifyProbingPaths configuration setting and set it to the folder where peverify.exe is located");
+		}
+
+		public static string PeVerifyPath { get; set; }
+	}
+#endif
+
+	public abstract class BasePEVerifyTestCase
+	{
+		protected ProxyGenerator generator;
+		protected IProxyBuilder builder;
+
+		private bool verificationDisabled;
+
+		[SetUp]
+		public virtual void Init()
+		{
+			ResetGeneratorAndBuilder();
+			verificationDisabled = false;
+		}
+
+		public void ResetGeneratorAndBuilder()
+		{
+#if SILVERLIGHT // no PersistentProxyBuilder in Silverlight
+			builder = new DefaultProxyBuilder();
+#else
+			builder = new PersistentProxyBuilder();
+#endif
+			generator = new ProxyGenerator(builder);
+		}
+
+		public void DisableVerification()
+		{
+			verificationDisabled = true;
+		}
+
+		public bool IsVerificationDisabled
+		{
+			get { return verificationDisabled; }
+		}
+
+#if !MONO && !SILVERLIGHT
+		// mono doesn't have PEVerify
+		[TearDown]
+		public virtual void TearDown()
+		{
+			if (!IsVerificationDisabled)
+			{
+				// Note: only supports one generated assembly at the moment
+				var path = ((PersistentProxyBuilder)builder).SaveAssembly();
+				if (path != null)
+				{
+					RunPEVerifyOnGeneratedAssembly(path);
+				}
+			}
+		}
+
+		public void RunPEVerifyOnGeneratedAssembly(string assemblyPath)
+		{
+			var process = new Process
+			{
+				StartInfo =
+				{
+					FileName = FindPeVerify.PeVerifyPath,
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory,
+					Arguments = "\"" + assemblyPath + "\" /VERBOSE",
+					CreateNoWindow = true
+				}
+			};
+			process.Start();
+			var processOutput = process.StandardOutput.ReadToEnd();
+			process.WaitForExit();
+
+			var result = process.ExitCode + " code ";
+
+			Console.WriteLine(GetType().FullName + ": " + result);
+
+			if (process.ExitCode != 0)
+			{
+				Console.WriteLine(processOutput);
+				Assert.Fail("PeVerify reported error(s): " + Environment.NewLine + processOutput, result);
+			}
+		}
+#else
+#if !SILVERLIGHT
+		[TearDown]
+#endif
+		public virtual void TearDown()
+		{
+		}
+#endif
+	}
+}
 namespace Castle.DynamicProxy.Tests
 {
 	using System;
