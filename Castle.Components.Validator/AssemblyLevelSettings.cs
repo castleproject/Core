93c27afdf6fbd68129d71b41fa5badda617dd3e0
hammett <hammett@gmail.com>
2011-10-23 03:35:26 -0700

    Reorg folders

--- Castle.Components.Validator/AssemblyLevelSettings.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Security;
-
-//For some odd reason, it behaves different in a win2k3 box.
-//[assembly: AllowPartiallyTrustedCallers]
-//[assembly: SecurityTransparent]
---
+++ Core/src/Castle.Components.Validator/AssemblyLevelSettings.cs
@@ -0,0 +1,5 @@
+﻿using System.Security;
+
+//For some odd reason, it behaves different in a win2k3 box.
+//[assembly: AllowPartiallyTrustedCallers]
+//[assembly: SecurityTransparent]
