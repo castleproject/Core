62a4f794c82c7fcd50fa6a5dc93e1c440676ac22
Jeff Sharp <jeff.sharp@improvingenterprises.com>
2011-12-19 10:52:32 -0600

    Use weak references in XmlReferenceManager EntryValue structure.

--- src/Castle.Core/Components.DictionaryAdapter/Xml/Core/XmlReferenceManager.cs
+++ src/Castle.Core/Components.DictionaryAdapter/Xml/Core/XmlReferenceManager.cs
@@ -341,7 +341,11 @@ namespace Castle.Components.DictionaryAdapter.Xml
 		private void PrepareForReuse(Entry entry)
 		{
 			foreach (var item in entry.Values)
-				entriesByValue.Remove(item.Value);
+			{
+				var value = item.Value.Target;
+				if (null != value)
+					entriesByValue.Remove(value);
+			}
 			entry.Values.Clear();
 
 			format.ClearIdentity(entry.Node);
@@ -381,12 +385,16 @@ namespace Castle.Components.DictionaryAdapter.Xml
 				if (!item.IsInGraph)
 					continue;
 
-				if (type.IsAssignableFrom(item.Type))
-					return Try.Success(out value, item.Value);
+				var candidate = item.Value.Target;
+				if (candidate == null)
+					continue;
 
-				var candidate = item.Value as IDictionaryAdapter;
-				if (candidate != null)
-					dictionaryAdapter = candidate;
+				if (type.IsAssignableFrom(item.Type))
+					if (null != candidate)
+						return Try.Success(out value, candidate);
+
+				if (dictionaryAdapter == null)
+					dictionaryAdapter = candidate as IDictionaryAdapter;
 			}
 
 			// Fall back to coercing a DA found in the graph
@@ -421,9 +429,11 @@ namespace Castle.Components.DictionaryAdapter.Xml
 			var values = entry.Values;
 			for (int index = 0; index < values.Count; index++)
 			{
-				if (ReferenceEquals(values[index].Value, value))
+				var item      = values[index];
+				var candidate = item.Value.Target;
+
+				if (ReferenceEquals(candidate, value))
 				{
-					var item = values[index];
 					item = new EntryValue(item.Type, item.Value, false);
 					values[index] = item;
 					return;
@@ -457,11 +467,12 @@ namespace Castle.Components.DictionaryAdapter.Xml
 
 					foreach (var otherItem in otherEntry.Values)
 					{
-						if (otherItem.Value == keyValue)
-							continue;
-						if (entriesByValue.ContainsKey(otherItem.Value))
-							continue;
-						AddValueCore(thisEntry, otherItem.Type, otherItem.Value, false);
+						var otherValue = otherItem.Value.Target;
+						if (otherValue == null     ||
+							otherValue == keyValue ||
+							entriesByValue.ContainsKey(otherValue))
+							{ continue; }
+						AddValueCore(thisEntry, otherItem.Type, otherValue, false);
 					}
 				}
 			}
@@ -546,11 +557,14 @@ namespace Castle.Components.DictionaryAdapter.Xml
 
 		private struct EntryValue
 		{
-			public readonly Type   Type;
-			public readonly object Value;
-			public readonly	bool   IsInGraph;
+			public readonly Type          Type;
+			public readonly WeakReference Value;
+			public readonly	bool          IsInGraph;
 
 			public EntryValue(Type type, object value, bool isInGraph)
+				: this(type, new WeakReference(value), isInGraph) { }
+
+			public EntryValue(Type type, WeakReference value, bool isInGraph)
 			{
 				Type      = type;
 				Value     = value;
