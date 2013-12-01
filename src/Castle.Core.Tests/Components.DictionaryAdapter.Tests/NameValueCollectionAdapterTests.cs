5f475214388f4605e01f4487915664671f84b4c6
Krzysztof Kozmic <krzysztof.kozmic@gmail.com>
2013-12-02 08:25:58 +1000

    formatting

--- src/Castle.Core.Tests/Components.DictionaryAdapter.Tests/NameValueCollectionAdapterTests.cs
+++ src/Castle.Core.Tests/Components.DictionaryAdapter.Tests/NameValueCollectionAdapterTests.cs
@@ -1,24 +1,40 @@
-﻿
+﻿// Copyright 2004-2013 Castle Project - http://www.castleproject.org/
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 namespace CastleTests.Components.DictionaryAdapter.Tests
 {
 	using System.Collections.Specialized;
-	using NUnit.Framework;
+
 	using Castle.Components.DictionaryAdapter;
+
+	using NUnit.Framework;
+
 	[TestFixture]
 	public class NameValueCollectionAdapterTests
 	{
-		private NameValueCollection nameValueCollection;
-
 		[SetUp]
 		public void SetUp()
 		{
 			nameValueCollection = new NameValueCollection();
 		}
 
+		private NameValueCollection nameValueCollection;
+
 		[Test]
 		public void Contains_IsCaseInsensitive()
 		{
-			NameValueCollectionAdapter adapter = new NameValueCollectionAdapter(nameValueCollection);
+			var adapter = new NameValueCollectionAdapter(nameValueCollection);
 			adapter["a key"] = "a value";
 
 			Assert.IsTrue(adapter.Contains("A Key"));
@@ -27,7 +43,7 @@ namespace CastleTests.Components.DictionaryAdapter.Tests
 		[Test]
 		public void Contains_IsCorrectWhenValueIsNull()
 		{
-			NameValueCollectionAdapter adapter = new NameValueCollectionAdapter(nameValueCollection);
+			var adapter = new NameValueCollectionAdapter(nameValueCollection);
 			adapter["a key"] = null;
 
 			Assert.IsTrue(adapter.Contains("A Key"));
