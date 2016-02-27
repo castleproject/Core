c770a822a8e51aa8d8061938f03b41dbe23f9f90
Blair Conrad <blair@blairconrad.com>
2016-02-27 16:50:32 -0500

    Add NETCORE build target
    Downloads dnvm, dnx, and required packages, then builds and tests.

---
+++ buildscripts/Build-NetCore.ps1
@@ -0,0 +1,39 @@
+$frameworkVersion = "1.0.0-rc1-update1"
+$frameworkArchitecture = "x86"
+
+$dnxHome = Join-Path (Get-Location) ".dnx"
+$dnxPath = Join-Path $dnxHome "bin"
+
+Write-Host "Downloading dnvm to: $dnxPath"
+if (!(Test-Path $dnxPath)) { md $dnxPath | Out-Null }
+
+$dnvmPs1Path = Join-Path $dnxPath "dnvm.ps1"
+$dnvmCmdPath = Join-Path $dnxPath "dnvm.cmd"
+
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile('https://raw.githubusercontent.com/aspnet/Home/dev/dnvm.ps1', $dnvmPs1Path)
+$wc.DownloadFile('https://raw.githubusercontent.com/aspnet/Home/dev/dnvm.cmd', $dnvmCmdPath)
+
+Write-Host "Downloading dnx"
+
+$env:PATH = ("$dnxPath;" + $env:PATH)
+
+dnvm install $frameworkVersion -runtime coreclr -arch $frameworkArchitecture
+
+dnx --version
+
+Write-Host "Downloading packages"
+
+dnu restore
+
+Write-Host "Building"
+
+$env:Path = ((Join-Path $dnxHome "runtimes\dnx-coreclr-win-$frameworkArchitecture.$frameworkVersion\bin") + ";" + $env:Path)
+
+dnu build src/Castle.Core src/Castle.Core.Tests --configuration Release --out build/NETCORE
+
+Write-Host "Running tests"
+
+dnx -p src/Castle.Core.Tests test
+
+exit $LASTEXITCODE
