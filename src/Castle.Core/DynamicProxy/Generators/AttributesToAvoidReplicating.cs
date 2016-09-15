92fd4c8efd2957408e1481fb0d302a6d096a7ee6
Ronny Borchert <ronny.borchert@muehlbauer.de>
2016-09-15 13:00:16 +0200

    merge in Core changes

--- src/Castle.Core/DynamicProxy/Generators/AttributesToAvoidReplicating.cs
+++ src/Castle.Core/DynamicProxy/Generators/AttributesToAvoidReplicating.cs
@@ -26,9 +26,10 @@ namespace Castle.DynamicProxy.Generators
 		static AttributesToAvoidReplicating()
 		{
 			Add<ComImportAttribute>();
+            Add<SecurityPermissionAttribute>();
 #if !SILVERLIGHT
-			Add<SecurityPermissionAttribute>();
-            Add<UIPermissionAttribute>();
+            //Add<SecurityPermissionAttribute>();
+            //Add<UIPermissionAttribute>();
 #endif
 #if DOTNET40
 			Add<TypeIdentifierAttribute>();
