162a8c07070733ff064340db8558fb99073306b1
Blair Conrad <blair@blairconrad.com>
2017-04-18 21:31:15 -0400

    Fix build errors introduced in #236

--- src/Castle.Core.Tests/DynamicProxy.Tests/ProxyUtilTestCase.cs
+++ src/Castle.Core.Tests/DynamicProxy.Tests/ProxyUtilTestCase.cs
@@ -64,12 +64,12 @@ namespace Castle.DynamicProxy.Tests
 			string reason;
 			ProxyUtil.IsAccessible(GetMethod<TestClass>(methodName), out reason);
 
-			var expectedReason =
-				"Can not create proxy for method Void APrivateMethod() " +
-				"because it is not accessible. Make it public, or internal " +
-				"and mark your assembly with " +
-				"[assembly: InternalsVisibleTo(InternalsVisible.ToDynamicProxyGenAssembly2)] " +
-				"attribute, because assembly Castle.Core.Tests is strong-named.";
+#if FEATURE_GET_REFERENCED_ASSEMBLIES
+			var expectedReason = "Can not create proxy for method Void APrivateMethod() because it is not accessible. Make it public, or internal and mark your assembly with [assembly: InternalsVisibleTo(InternalsVisible.ToDynamicProxyGenAssembly2)] attribute, because assembly Castle.Core.Tests is strong-named.";
+#else
+			var expectedReason = "Can not create proxy for method Void APrivateMethod() because it is not accessible. Make it public, or internal and mark your assembly with [assembly: InternalsVisibleTo(\"DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7\")] attribute, because assembly Castle.Core.Tests is strong-named.";
+#endif
+
 			Assert.AreEqual(expectedReason, reason);
 		}
 
@@ -79,7 +79,7 @@ namespace Castle.DynamicProxy.Tests
 				BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 		}
 
-		private static readonly object[] AccessibleMethods =
+		public static readonly object[] AccessibleMethods =
 		{
 			new object[] { "APublicMethod" },
 			new object[] { "AProtectedMethod" },
@@ -87,7 +87,7 @@ namespace Castle.DynamicProxy.Tests
 			new object[] { "AProtectedInternalMethod" }
 		};
 
-		private static readonly object[] InaccessibleMethods =
+		public static readonly object[] InaccessibleMethods =
 		{
 			new object[] { "APrivateMethod" },
 		};
