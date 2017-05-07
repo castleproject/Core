7b0d025beff9c257aa962fab40b2a840856ae4c0
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2017-05-07 21:03:35 +0100

    Added comment to build.sh about dotnetframework targeting for dektopclr on linux builds

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ if [ ! -f "$MONOPATH" ]; then
 fi
 
 # This installs the latest build of the dotnet targetting pack net461 via NuGet for linux desktop clr builds. 
-# The package source can be removed once Microsoft.TargetingPack.NETFramework.v4.6.1 comes out of alpha. 
+# The package source can be removed once Microsoft.TargetingPack.NETFramework.v4.6.1 comes out of alpha on NuGet. 
 dotnet restore ./buildscripts/BuildScripts.csproj -s https://dotnet.myget.org/F/dotnet-core/api/v3/index.json
 dotnet restore ./src/Castle.Core/Castle.Core-VS2017.csproj
 dotnet restore ./src/Castle.Services.Logging.log4netIntegration/Castle.Services.Logging.log4netIntegration-VS2017.csproj
