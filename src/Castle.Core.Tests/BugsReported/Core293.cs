85c51393d9c4898767b49d735ff03c07a5cbe541
yallie <yallie@yandex.ru>
2017-08-15 00:18:57 +0300

    Added a failing test for the generic interfaces and ChangeProxyTarget, #293.

---
+++ src/Castle.Core.Tests/BugsReported/Core293.cs
@@ -0,0 +1,97 @@
+﻿// Copyright 2004-2017 Castle Project - http://www.castleproject.org/
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Castle.DynamicProxy.Tests.BugsReported
+{
+	using System;
+
+	using NUnit.Framework;
+
+	public class LazyInterceptor<T> : IInterceptor
+		where T : class
+	{
+		public LazyInterceptor(Lazy<T> lazyTarget)
+		{
+			LazyTarget = lazyTarget;
+		}
+
+		private Lazy<T> LazyTarget { get; }
+
+		public void Intercept(IInvocation invocation)
+		{
+			var target = invocation.InvocationTarget as T;
+			if (target == null)
+			{
+				(invocation as IChangeProxyTarget).ChangeInvocationTarget(LazyTarget.Value);
+				(invocation as IChangeProxyTarget).ChangeProxyTarget(LazyTarget.Value);
+			}
+
+			invocation.Proceed();
+		}
+	}
+
+	public interface IEventHandler
+	{
+		string Handle(EventArgs ea);
+	}
+
+	public interface IEventHandler<T> : IEventHandler
+		where T : EventArgs
+	{
+	}
+
+	public class EventArgs1 : EventArgs { }
+
+	public class Handler1 : IEventHandler<EventArgs1>
+	{
+		public string Handle(EventArgs ea)
+		{
+			return "Handler1";
+		}
+	}
+
+	public class EventArgs2 : EventArgs { }
+
+	public class Handler2 : IEventHandler<EventArgs2>
+	{
+		public string Handle(EventArgs ea)
+		{
+			return "Handler2";
+		}
+	}
+
+	[TestFixture]
+	public class Core293
+	{
+		[Test]
+		public void ShouldChangeTargetProxyForGenericInterfaces()
+		{
+			var generator = new ProxyGenerator();
+
+			var lazyTarget1 = new Lazy<IEventHandler<EventArgs1>>(() => new Handler1());
+			var lazyInterceptor1 = new LazyInterceptor<IEventHandler<EventArgs1>>(lazyTarget1);
+			var proxy1 = generator.CreateInterfaceProxyWithTargetInterface<IEventHandler<EventArgs1>>(null, new[] { lazyInterceptor1 });
+
+			var result1 = proxy1.Handle(EventArgs.Empty);
+			Assert.AreEqual("Handler1", result1);
+
+			var lazyTarget2 = new Lazy<IEventHandler<EventArgs2>>(() => new Handler2());
+			var lazyInterceptor2 = new LazyInterceptor<IEventHandler<EventArgs2>>(lazyTarget2);
+			var proxy2 = generator.CreateInterfaceProxyWithTargetInterface<IEventHandler<EventArgs2>>(null, new[] { lazyInterceptor2 });
+
+			var result2 = proxy2.Handle(EventArgs.Empty);
+			Assert.AreEqual("Handler2", result2);
+		}
+	}
+}
