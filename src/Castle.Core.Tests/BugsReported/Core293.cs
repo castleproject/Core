a548018ffe8a812c61729a882cfda845c07baf0b
yallie <yallie@yandex.ru>
2017-09-19 18:57:03 +0300

    Marked IChangeProxyTarget.ChangeProxyTarget method as obsolete in favor of IProxyTargetAccessor.DynProxySetTarget, #293.

--- src/Castle.Core.Tests/BugsReported/Core293.cs
+++ src/Castle.Core.Tests/BugsReported/Core293.cs
@@ -18,10 +18,10 @@ namespace Castle.DynamicProxy.Tests.BugsReported
 
 	using NUnit.Framework;
 
-	public class LazyInterceptor<T> : IInterceptor
+	public class LazyInterceptorV1<T> : IInterceptor
 		where T : class
 	{
-		public LazyInterceptor(Lazy<T> lazyTarget)
+		public LazyInterceptorV1(Lazy<T> lazyTarget)
 		{
 			LazyTarget = lazyTarget;
 		}
@@ -34,7 +34,32 @@ namespace Castle.DynamicProxy.Tests.BugsReported
 			if (target == null)
 			{
 				(invocation as IChangeProxyTarget).ChangeInvocationTarget(LazyTarget.Value);
+				(invocation.Proxy as IProxyTargetAccessor).DynProxySetTarget(LazyTarget.Value);
+			}
+
+			invocation.Proceed();
+		}
+	}
+
+	public class LazyInterceptorV2<T> : IInterceptor
+		where T : class
+	{
+		public LazyInterceptorV2(Lazy<T> lazyTarget)
+		{
+			LazyTarget = lazyTarget;
+		}
+
+		private Lazy<T> LazyTarget { get; }
+
+		public void Intercept(IInvocation invocation)
+		{
+			var target = invocation.InvocationTarget as T;
+			if (target == null)
+			{
+				(invocation as IChangeProxyTarget).ChangeInvocationTarget(LazyTarget.Value);
+#pragma warning disable CS0618 // obsolete: use DynProxySetTarget instead
 				(invocation as IChangeProxyTarget).ChangeProxyTarget(LazyTarget.Value);
+#pragma warning restore CS0618 // obsolete
 			}
 
 			invocation.Proceed();
@@ -82,27 +107,52 @@ namespace Castle.DynamicProxy.Tests.BugsReported
 	}
 
 	[TestFixture]
-	public class Core293 : BasePEVerifyTestCase
+	public class ChangeProxyTargetTestCase : BasePEVerifyTestCase
 	{
 		[Test]
-		public void ShouldChangeTargetProxyForGenericInterfaces()
+		public void Should_Change_Target_Proxy_For_Generic_Interfaces_Recommended_Approach()
 		{
 			var lazyTarget1 = new Lazy<IEventHandler<EventArgs1>>(() => new Handler1());
-			var lazyInterceptor1 = new LazyInterceptor<IEventHandler<EventArgs1>>(lazyTarget1);
+			var lazyInterceptor1 = new LazyInterceptorV1<IEventHandler<EventArgs1>>(lazyTarget1);
 			var proxy1 = generator.CreateInterfaceProxyWithTargetInterface<IEventHandler<EventArgs1>>(null, new[] { lazyInterceptor1 });
 
 			var result1 = proxy1.Handle(EventArgs.Empty);
 			Assert.AreEqual("Handler1", result1);
 
 			var lazyTarget2 = new Lazy<IEventHandler<EventArgs2>>(() => new Handler2());
-			var lazyInterceptor2 = new LazyInterceptor<IEventHandler<EventArgs2>>(lazyTarget2);
+			var lazyInterceptor2 = new LazyInterceptorV1<IEventHandler<EventArgs2>>(lazyTarget2);
 			var proxy2 = generator.CreateInterfaceProxyWithTargetInterface<IEventHandler<EventArgs2>>(null, new[] { lazyInterceptor2 });
 
 			var result2 = proxy2.Handle(EventArgs.Empty);
 			Assert.AreEqual("Handler2", result2);
 
 			var lazyTarget3 = new Lazy<IEventHandler3>(() => new Handler3());
-			var lazyInterceptor3 = new LazyInterceptor<IEventHandler3>(lazyTarget3);
+			var lazyInterceptor3 = new LazyInterceptorV1<IEventHandler3>(lazyTarget3);
+			var proxy3 = generator.CreateInterfaceProxyWithTargetInterface<IEventHandler3>(null, new[] { lazyInterceptor3 });
+
+			var result3 = proxy3.Handle(EventArgs.Empty);
+			Assert.AreEqual("Handler3", result3);
+		}
+
+		[Test]
+		public void Should_Change_Target_Proxy_For_Generic_Interfaces_Obsolete_Approach()
+		{
+			var lazyTarget1 = new Lazy<IEventHandler<EventArgs1>>(() => new Handler1());
+			var lazyInterceptor1 = new LazyInterceptorV2<IEventHandler<EventArgs1>>(lazyTarget1);
+			var proxy1 = generator.CreateInterfaceProxyWithTargetInterface<IEventHandler<EventArgs1>>(null, new[] { lazyInterceptor1 });
+
+			var result1 = proxy1.Handle(EventArgs.Empty);
+			Assert.AreEqual("Handler1", result1);
+
+			var lazyTarget2 = new Lazy<IEventHandler<EventArgs2>>(() => new Handler2());
+			var lazyInterceptor2 = new LazyInterceptorV2<IEventHandler<EventArgs2>>(lazyTarget2);
+			var proxy2 = generator.CreateInterfaceProxyWithTargetInterface<IEventHandler<EventArgs2>>(null, new[] { lazyInterceptor2 });
+
+			var result2 = proxy2.Handle(EventArgs.Empty);
+			Assert.AreEqual("Handler2", result2);
+
+			var lazyTarget3 = new Lazy<IEventHandler3>(() => new Handler3());
+			var lazyInterceptor3 = new LazyInterceptorV2<IEventHandler3>(lazyTarget3);
 			var proxy3 = generator.CreateInterfaceProxyWithTargetInterface<IEventHandler3>(null, new[] { lazyInterceptor3 });
 
 			var result3 = proxy3.Handle(EventArgs.Empty);
