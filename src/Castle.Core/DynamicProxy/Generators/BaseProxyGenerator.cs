91b091cccf1eaae499094213198bd1daecae540e
stakx <stakx@eml.cc>
2018-06-21 00:38:29 +0200

    Replace `Scope.Lock` in `BaseProxyGenerator`
    This commit removes the last actual usage of a `Lock` inside the
    library.

--- src/Castle.Core/DynamicProxy/Generators/BaseProxyGenerator.cs
+++ src/Castle.Core/DynamicProxy/Generators/BaseProxyGenerator.cs
@@ -384,40 +384,25 @@ namespace Castle.DynamicProxy.Generators
 
 		protected Type ObtainProxyType(CacheKey cacheKey, Func<string, INamingScope, Type> factory)
 		{
-			Type cacheType;
-			using (var locker = Scope.Lock.ForReading())
-			{
-				cacheType = GetFromCache(cacheKey);
-				if (cacheType != null)
-				{
-					Logger.DebugFormat("Found cached proxy type {0} for target type {1}.", cacheType.FullName, targetType.FullName);
-					return cacheType;
-				}
-			}
+			bool notFoundInTypeCache = false;
 
-			// This is to avoid generating duplicate types under heavy multithreaded load.
-			using (var locker = Scope.Lock.ForWriting())
+			var proxyType = Scope.GetOrAddToCache(cacheKey, _ =>
 			{
-				// Only one thread at a time may enter a write lock.
-				// See if an earlier lock holder populated the cache.
-				cacheType = GetFromCache(cacheKey);
-				if (cacheType != null)
-				{
-					Logger.DebugFormat("Found cached proxy type {0} for target type {1}.", cacheType.FullName, targetType.FullName);
-					return cacheType;
-				}
-
-				// Log details about the cache miss
+				notFoundInTypeCache = true;
 				Logger.DebugFormat("No cached proxy type was found for target type {0}.", targetType.FullName);
+
 				EnsureOptionsOverrideEqualsAndGetHashCode(ProxyGenerationOptions);
 
 				var name = Scope.NamingScope.GetUniqueName("Castle.Proxies." + targetType.Name + "Proxy");
-				var proxyType = factory.Invoke(name, Scope.NamingScope.SafeSubScope());
+				return factory.Invoke(name, Scope.NamingScope.SafeSubScope());
+			});
 
-				AddToCache(cacheKey, proxyType);
-
-				return proxyType;
+			if (!notFoundInTypeCache)
+			{
+				Logger.DebugFormat("Found cached proxy type {0} for target type {1}.", proxyType.FullName, targetType.FullName);
 			}
+
+			return proxyType;
 		}
 
 		private bool IsConstructorVisible(ConstructorInfo constructor)
