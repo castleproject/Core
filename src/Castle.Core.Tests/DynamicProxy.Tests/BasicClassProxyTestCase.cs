5bb79fbcccd3bd6fb1a6eb23073f61dc2f040f7b
mriehm <3916550+mriehm@users.noreply.github.com>
2020-04-28 20:36:24 -0500

    Update BasicClassProxyTestCase.cs

--- src/Castle.Core.Tests/DynamicProxy.Tests/BasicClassProxyTestCase.cs
+++ src/Castle.Core.Tests/DynamicProxy.Tests/BasicClassProxyTestCase.cs
@@ -1,7 +1,3 @@
-//----------------------------------------------------
-// Copyright 2020 Epic Systems Corporation
-//----------------------------------------------------
-
 // Copyright 2004-2016 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
