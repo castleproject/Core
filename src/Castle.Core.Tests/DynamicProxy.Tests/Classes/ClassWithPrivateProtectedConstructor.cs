f5cae9e0629e062665f2982485e5f0cb68ff07ce
Mark Riehm <mriehm@epic.com>
2020-04-28 20:25:18 -0500

    Make private protected constructors consistent with protected and internal constructors

---
+++ src/Castle.Core.Tests/DynamicProxy.Tests/Classes/ClassWithPrivateProtectedConstructor.cs
@@ -0,0 +1,28 @@
+﻿//----------------------------------------------------
+// Copyright 2020 Epic Systems Corporation
+//----------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Castle.DynamicProxy.Tests.Classes
+{
+	public class ClassWithPrivateProtectedConstructor
+	{
+		private protected ClassWithPrivateProtectedConstructor()
+		{
+			_someString = "Something";
+		}
+
+		private string _someString = string.Empty;
+
+		public string SomeString
+		{
+			get { return _someString; }
+			set { _someString = value; }
+		}
+	}
+}
