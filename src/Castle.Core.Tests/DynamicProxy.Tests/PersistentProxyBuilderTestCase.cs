36cd1d4dc6b9ba5130db5a95cf23d79b5b21cfb5
stakx <stakx@eml.cc>
2020-06-12 19:47:00 +0200

    Replace incorrect `FEATURE_SERIALIZATION` guard

--- src/Castle.Core.Tests/DynamicProxy.Tests/PersistentProxyBuilderTestCase.cs
+++ src/Castle.Core.Tests/DynamicProxy.Tests/PersistentProxyBuilderTestCase.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if FEATURE_SERIALIZATION
+#if FEATURE_ASSEMBLYBUILDER_SAVE
 
 namespace Castle.DynamicProxy.Tests
 {
