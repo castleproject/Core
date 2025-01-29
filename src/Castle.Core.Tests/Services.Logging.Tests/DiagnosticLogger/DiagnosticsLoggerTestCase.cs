27b24950567ca52b933e99ab9c7aef5ed3895d01
Rolf Kristensen <sweaty1@hotmail.com>
2025-01-29 21:33:32 +0100

    Castle.Services.Logging.EventLogIntegration for Windows EventLog

--- src/Castle.Core.Tests/Services.Logging.Tests/DiagnosticLogger/DiagnosticsLoggerTestCase.cs
+++ src/Castle.Core.Tests/Services.Logging.Tests/DiagnosticLogger/DiagnosticsLoggerTestCase.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Castle.Core.Logging.Tests
+namespace Castle.Services.Logging.Tests
 {
 	using System;
 	using System.Diagnostics;
