318fe3b9637461a9db2707adcc6f10fe7bbaae59
Rolf Kristensen <sweaty1@hotmail.com>
2025-02-16 02:51:18 +0100

    Public API updates

---
+++ ref/Castle.Services.Logging.EventLogIntegration-netstandard2.1.cs
@@ -0,0 +1,22 @@
+[assembly: System.Reflection.AssemblyMetadata("RepositoryUrl", "https://github.com/castleproject/Core")]
+[assembly: System.Runtime.Versioning.TargetFramework(".NETStandard,Version=v2.1", FrameworkDisplayName=".NET Standard 2.1")]
+namespace Castle.Services.Logging
+{
+    public class DiagnosticsLogger : Castle.Core.Logging.LevelFilteredLogger, System.IDisposable
+    {
+        public DiagnosticsLogger(string logName) { }
+        public DiagnosticsLogger(string logName, string source) { }
+        public DiagnosticsLogger(string logName, string machineName, string source) { }
+        public override Castle.Core.Logging.ILogger CreateChildLogger(string loggerName) { }
+        public void Dispose() { }
+        protected virtual void Dispose(bool disposing) { }
+        protected override void Finalize() { }
+        protected override void Log(Castle.Core.Logging.LoggerLevel loggerLevel, string loggerName, string message, System.Exception exception) { }
+    }
+    public class DiagnosticsLoggerFactory : Castle.Core.Logging.AbstractLoggerFactory
+    {
+        public DiagnosticsLoggerFactory() { }
+        public override Castle.Core.Logging.ILogger Create(string name) { }
+        public override Castle.Core.Logging.ILogger Create(string name, Castle.Core.Logging.LoggerLevel level) { }
+    }
+}
