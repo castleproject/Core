4cb2e891252b06a689b33b8c84287cfb93de6bd8
Dominique Schuppli <stakx@eml.cc>
2025-12-17 21:42:49 +0100

    Remove redundant `AssignArgumentStatement`
    ... which only works correctly for by-refs and with `DefaultValue-
    Expression`

--- src/Castle.Core/DynamicProxy/Generators/MinimalisticMethodGenerator.cs
+++ src/Castle.Core/DynamicProxy/Generators/MinimalisticMethodGenerator.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2004-2021 Castle Project - http://www.castleproject.org/
+﻿// Copyright 2004-2025 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -52,8 +52,10 @@ namespace Castle.DynamicProxy.Generators
 				if (parameter.IsOut)
 				{
 					emitter.CodeBuilder.AddStatement(
-						new AssignArgumentStatement(new ArgumentReference(parameter.ParameterType, index + 1),
-						                            new DefaultValueExpression(parameter.ParameterType)));
+						new AssignStatement(
+							new IndirectReference(
+								new ArgumentReference(parameter.ParameterType, index + 1)),
+							new DefaultValueExpression(parameter.ParameterType.GetElementType())));
 				}
 			}
 		}
