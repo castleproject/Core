58e8236941e58963307bf3dced087caf7339a19b
Dominique Schuppli <stakx@eml.cc>
2025-12-23 19:27:59 +0100

    Don't compare type members case-insensitively

--- src/Castle.Core/DynamicProxy/Generators/MetaMethod.cs
+++ src/Castle.Core/DynamicProxy/Generators/MetaMethod.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2021 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2025 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -61,7 +61,7 @@ namespace Castle.DynamicProxy.Generators
 				return true;
 			}
 
-			if (!StringComparer.OrdinalIgnoreCase.Equals(Name, other.Name))
+			if (!StringComparer.Ordinal.Equals(Name, other.Name))
 			{
 				return false;
 			}
