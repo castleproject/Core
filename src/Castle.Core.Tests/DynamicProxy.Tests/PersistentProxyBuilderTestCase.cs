2d073b974b127ca92f8135380e80d016bc4864ba
Dominique Schuppli <stakx@eml.cc>
2025-12-31 03:16:04 +0100

    Add tests for `PersistentProxyBuilder`

--- src/Castle.Core.Tests/DynamicProxy.Tests/PersistentProxyBuilderTestCase.cs
+++ src/Castle.Core.Tests/DynamicProxy.Tests/PersistentProxyBuilderTestCase.cs
@@ -1,4 +1,4 @@
-// Copyright 2004-2021 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2026 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,13 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if FEATURE_ASSEMBLYBUILDER_SAVE
-
 namespace Castle.DynamicProxy.Tests
 {
 	using System;
+	using System.Collections.Generic;
 	using System.IO;
+	using System.Linq;
+	using System.Reflection;
+
+	using Castle.DynamicProxy.Tests.Interfaces;
+
 	using NUnit.Framework;
+
+#if NET462_OR_GREATER
 
 	[TestFixture]
 	public class PersistentProxyBuilderTestCase
@@ -44,6 +50,55 @@ namespace Castle.DynamicProxy.Tests
 			Assert.IsTrue(path.EndsWith(ModuleScope.DEFAULT_FILE_NAME));
 		}
 	}
-}
+
+#elif NET9_0_OR_GREATER
+
+	[TestFixture]
+	[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+	public class PersistentProxyBuilderTestCase
+	{
+		private List<Assembly> assemblies;
+		private PersistentProxyBuilder builder;
+
+		[SetUp]
+		public void SetupProxyBuilder()
+		{
+			assemblies = new List<Assembly>();
+			builder = new PersistentProxyBuilder();
+			builder.AssemblyCreated += (object _, PersistentProxyBuilderAssemblyEventArgs e) =>
+			{
+				assemblies.Add(e.Assembly);
+			};
+		}
+
+		[Test]
+		public void SavesOneAssemblyPerProxiedType()
+		{
+			var oneProxyType = builder.CreateInterfaceProxyTypeWithoutTarget(typeof(IOne), Type.EmptyTypes, ProxyGenerationOptions.Default);
+			Assert.AreEqual(1, assemblies.Count);
+
+			var twoProxyType = builder.CreateInterfaceProxyTypeWithoutTarget(typeof(ITwo), Type.EmptyTypes, ProxyGenerationOptions.Default);
+			Assert.AreEqual(2, assemblies.Count);
+
+			var oneAssembly = assemblies[0];
+			var twoAssembly = assemblies[1];
+			Assert.AreSame(oneAssembly, oneProxyType.Assembly);
+			Assert.AreSame(twoAssembly, twoProxyType.Assembly);
+			Assert.AreNotSame(oneAssembly, twoAssembly);
+		}
+
+		[Test]
+		public void TypeCacheWorks()
+		{
+			var proxyType1 = builder.CreateClassProxyType(typeof(object), Type.EmptyTypes, ProxyGenerationOptions.Default);
+			var proxyType2 = builder.CreateClassProxyType(typeof(object), Type.EmptyTypes, ProxyGenerationOptions.Default);
+
+			Assert.AreEqual(1, assemblies.Count);
+			Assert.AreSame(proxyType1, proxyType2);
+			Assert.AreSame(proxyType1.Assembly, proxyType2.Assembly);
+		}
+	}
 
 #endif
+
+}
